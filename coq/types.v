ce229a371335300a7cce9b8e8669259f7dd14869
mrmr1993 <mr_1993@hotmail.co.uk>
2019-01-12 00:16:28 +0000

    Tweak types module

--- coq/types.v
+++ coq/types.v
@@ -1,4 +1,5 @@
 Require Import String.
+Require Import Snarky.typ_monads.
 
 Module Constraint.
   Module Basic.
@@ -34,63 +35,64 @@ Parameter provider : forall (a env s : Type), Type.
 
 Parameter handle : forall (var value : Type), Type.
 
-Module Store.
-  Inductive t (var f v : Type) := Store : f -> (v -> var) -> t var f v.
-End Store.
-
-Module Read.
-  Inductive t (value f v : Type) := Read : v -> (f -> value) -> t value f v.
-End Read.
-
-Module Alloc.
-  Inductive t (var v : Type) := Alloc : (v -> var) -> t var v.
-End Alloc.
-
 Module Types.
-  Inductive checked (f : Type) (v : Type) (sys : Type) :
-    forall (a s : Type), Type :=
-    | Pure a s : a -> checked f v sys a s
-    | Add_constraint a s : Constraint.t v -> checked f v sys a s
-    | As_prover a s :
-        As_prover.t f v sys unit ->
-        checked f v sys a s ->
-        checked f v sys a s
-    | With_label a b s :
-        string ->
-        checked f v sys a s ->
-        (a -> checked f v sys b s) ->
-        checked f v sys b s
-    | With_state a b s1 s2 :
-        As_prover.t f v s1 s2 ->
-        (s2 -> As_prover.t f v s1 unit) ->
-        checked b s2 f v sys ->
-        (b -> checked f v sys a s1) ->
-        checked f v sys a s1
-    | With_handler a b s :
-        request_handler ->
-        checked f v sys a s ->
-        (a -> checked f v sys b s) ->
-        checked f v sys b s
-    | Clear_handler a b s :
-      checked f v sys a s ->
-      (a -> checked f v sys b s) ->
-      checked f v sys b s
-    | Exists a s var value :
-      typ f v sys var value ->
-      provider value (v -> f) s ->
-      (handle var value -> checked f v sys a s) ->
-      checked f v sys a s
-    | Next_auxiliary a s :
-      (nat -> checked f v sys a s) -> checked f v sys a s
+  Section type.
+    Polymorphic Variables f v sys : Type.
 
-    with typ (f : Type) (v : Type) (sys : Type) :
-      forall (var : Type) (value : Type), Type :=
-    | Mk_typ var value :
-      forall (store : value -> Store.t var f v)
-      (read : var -> Read.t value f v)
-      (alloc : Alloc.t var v)
-      (check : var -> checked f v sys unit unit),
-        typ f v sys var value.
+    Polymorphic Inductive checked : forall (s a : Type), Type :=
+      | Pure a s : a -> checked s a
+      | Add_constraint a s : Constraint.t v -> checked s a -> checked s a
+      | As_prover a s :
+          As_prover.t f v sys unit ->
+          checked s a ->
+          checked s a
+      | With_label a b s :
+          string ->
+          checked s a ->
+          (a -> checked s b) ->
+          checked s b
+      | With_state a b s1 s2 :
+          As_prover.t f v s1 s2 ->
+          (s2 -> As_prover.t f v s1 unit) ->
+          checked s2 b ->
+          (b -> checked s1 a) ->
+          checked s1 a
+      | With_handler a b s :
+          request_handler ->
+          checked s a ->
+          (a -> checked s b) ->
+          checked s b
+      | Clear_handler a b s :
+        checked s a ->
+        (a -> checked s b) ->
+        checked s b
+      | Exists a s var value :
+        typ var value ->
+        provider value (v -> f) s ->
+        (handle var value -> checked s a) ->
+        checked s a
+      | Next_auxiliary a s :
+        (nat -> checked s a) -> checked s a
+  
+      with typ : forall (var : Type) (value : Type), Type :=
+      | Mk_typ var value :
+        forall (store : value -> Store.t f v var)
+        (read : var -> Read.t f v value)
+        (alloc : Alloc.t v var)
+        (check : var -> checked unit unit),
+          typ var value.
+  End type.
+
+  Arguments Pure {f v sys a s}.
+  Arguments Add_constraint {f v sys a s}.
+  Arguments As_prover {f v sys a s}.
+  Arguments With_label {f v sys a b s}.
+  Arguments With_state {f v sys a b s1 s2}.
+  Arguments With_handler {f v sys a b s}.
+  Arguments Clear_handler {f v sys a b s}.
+  Arguments Exists {f v sys a s var value}.
+  Arguments Next_auxiliary {f v sys a s}.
+  Arguments Add_constraint {f v sys a s}.
 End Types.
 
 Module Checked.
@@ -99,4 +101,24 @@ End Checked.
 
 Module Typ.
   Definition t f v sys var value := Types.typ f v sys var value.
+
+  Definition store {f v sys var value} (t : t f v sys var value) : value -> Store.t f v var :=
+    match t in Types.typ _ _ _ var value return value -> Store.t f v var with
+    | Types.Mk_typ _ _ _ _ _ store read alloc check => store
+    end.
+
+  Definition read {f v sys var value} (t : t f v sys var value) : var -> Read.t f v value :=
+    match t in Types.typ _ _ _ var value return var -> Read.t f v value with
+    | Types.Mk_typ _ _ _ _ _ store read alloc check => read
+    end.
+
+  Definition alloc {f v sys var value} (t : t f v sys var value) : Alloc.t v var :=
+    match t in Types.typ _ _ _ var value return Alloc.t v var with
+    | Types.Mk_typ _ _ _ _ _ store read alloc check => alloc
+    end.
+
+  Definition check {f v sys var value} (t : t f v sys var value) : var -> Checked.t f v sys unit unit :=
+    match t in Types.typ _ _ _ var value return var -> Checked.t f v sys unit unit with
+    | Types.Mk_typ _ _ _ _ _ store read alloc check => check
+    end.
 End Typ.
