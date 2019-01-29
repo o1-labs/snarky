b67e2095dd2578357fac7678ecff75a96e4de53f
mrmr1993 <mr_1993@hotmail.co.uk>
2019-01-29 13:46:18 +0000

    Add nicer error messages for the typechecker

--- dsl/typechecker.ml
+++ dsl/typechecker.ml
@@ -41,7 +41,7 @@ let rec check_type_aux ~defer_as typ constr_typ =
       ()
   | _, _ -> raise (Check_failed (typ, constr_typ))
 
-let check_type typ constr_typ =
+let check_type ~loc typ constr_typ =
   let typs = ref [] in
   let defer_as typ new_typ =
     typ.type_desc <- Tdefer new_typ ;
@@ -55,9 +55,24 @@ let check_type typ constr_typ =
         fixup_deferred typs
     | _ :: typs -> fixup_deferred typs
   in
-  check_type_aux ~defer_as typ constr_typ ;
-  fixup_deferred !typs ;
-  constr_typ
+  ( try check_type_aux ~defer_as typ constr_typ
+    with Check_failed (typ', constr_typ') ->
+      let open Format in
+      let pp_typ typ =
+        Pprintast.core_type err_formatter (To_ocaml.of_typ typ)
+      in
+      Location.print_error err_formatter loc ;
+      pp_print_string err_formatter " Type error: Cannot unify '" ;
+      pp_typ typ ;
+      pp_print_string err_formatter "' and '" ;
+      pp_typ constr_typ ;
+      pp_print_string err_formatter ", types '" ;
+      pp_typ typ' ;
+      pp_print_string err_formatter "' and '" ;
+      pp_typ constr_typ' ;
+      pp_print_string err_formatter "' are incompatable." ;
+      pp_print_newline err_formatter () ) ;
+  fixup_deferred !typs ; constr_typ
 
 type state =
   { map: (string, type_expr, Base.String.comparator_witness) Base.Map.t
@@ -113,18 +128,18 @@ let add_type_final name typ state =
   add_type name (mk (Tcopy (typ, state.depth))) state
 
 let add_type_in_progress name typ state =
-  typ.type_desc <- Tnocopy (mk typ.type_desc, state.depth);
+  typ.type_desc <- Tnocopy (mk typ.type_desc, state.depth) ;
   add_type name typ state
 
 let rec check_pattern ~add state typ pat =
   match pat.pat_desc with
   | PVariable str -> add str typ state
-  | PConstraint (p, constr_typ) -> (
-    match check_type typ constr_typ with
-    | typ -> check_pattern ~add state typ p
-    | exception Check_failed (_typ', _constr_typ') -> failwith "" )
+  | PConstraint (p, constr_typ) ->
+      let typ = check_type ~loc:pat.pat_loc typ constr_typ in
+      check_pattern ~add state typ p
 
 let rec get_expression state exp =
+  let loc = exp.exp_loc in
   match exp.exp_desc with
   | Apply (f, xs) ->
       let f_typ = get_expression state f in
@@ -133,18 +148,21 @@ let rec get_expression state exp =
         | [] -> f_typ
         | x :: xs -> (
             let x_typ = get_expression state x in
-            match check_type f_typ (mk (Tarrow (x_typ, mk (Tvar None)))) with
+            match
+              check_type ~loc f_typ
+                (mk ~loc (Tarrow (x_typ, mk ~loc (Tvar None))))
+            with
             | {type_desc= Tarrow (_, f_typ); _} -> apply_typ xs f_typ
             | _ -> failwith "Met constraint Tarrow, but didn't match Tarrow.."
             )
       in
       apply_typ xs f_typ
   | Variable name -> get_name name state
-  | Int _ -> mk (Tconstr {txt= "int"; loc= Location.none})
+  | Int _ -> mk (Tconstr {txt= "int"; loc})
   | Fun (p, body) ->
       (* In OCaml, function arguments can't be polymorphic, so each check refines
        them rather than instanciating the parameters. *)
-      let state = {state with depth = state.depth + 1} in
+      let state = {state with depth= state.depth + 1} in
       let p_typ = mk (Tvar None) in
       let state = check_pattern ~add:add_type_in_progress state p_typ p in
       let body_typ = get_expression state body in
@@ -157,7 +175,7 @@ let rec get_expression state exp =
       get_expression state e2
   | Constraint (e, typ) ->
       let e_typ = get_expression state e in
-      check_type e_typ typ
+      check_type ~loc e_typ typ
 
 and check_binding (state : 's) p e : 's =
   let e_type = get_expression state e in
