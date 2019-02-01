e9bb0b33b4fc0e35102e9a557bb84b509a7dd538
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-01 18:53:03 +0000

    Carry constructors in the environment

--- dsl/environ.ml
+++ dsl/environ.ml
@@ -1,5 +1,6 @@
 open Core_kernel
 open Parsetypes
+open Option.Let_syntax
 
 type 'a ident_table = (string, 'a, String.comparator_witness) Map.t
 
@@ -10,6 +11,7 @@ type t =
   ; typ_vars: ([`User | `Generated] * type_expr) ident_table
   ; types: type_decl ident_table
   ; fields: (type_expr * int) ident_table
+  ; constructors: (type_expr * int) ident_table
   ; match_instances: type_expr list list
   ; vars_size: int
   ; depth: int }
@@ -26,6 +28,9 @@ let pp (output : Format.formatter) (env : t) =
       Format.fprintf output "%s : %a\n" key pp_type_expr typ ) ;
   Format.pp_print_string output "Fields:\n" ;
   Map.iteri env.fields ~f:(fun ~key ~data:(typ, _) ->
+      Format.fprintf output "%s : %a\n" key pp_type_expr typ ) ;
+  Format.pp_print_string output "Constructors:\n" ;
+  Map.iteri env.constructors ~f:(fun ~key ~data:(typ, _) ->
       Format.fprintf output "%s : %a\n" key pp_type_expr typ )
 
 let pp_ocaml (output : Format.formatter) (env : t) =
@@ -58,6 +63,15 @@ let pp_ocaml (output : Format.formatter) (env : t) =
                 (Val.mk Location.(mkloc key none) (To_ocaml.of_typ typ))
               :: fields ) )
   in
+  let constructors =
+    doc_comment "Constructors:"
+    :: ( List.rev
+       @@ Map.fold env.constructors ~init:[]
+            ~f:(fun ~key ~data:(typ, _) constructors ->
+              Sig.value
+                (Val.mk Location.(mkloc key none) (To_ocaml.of_typ typ))
+              :: constructors ) )
+  in
   Pprintast.signature output (types @ values @ fields @ constructors)
 
 let empty =
@@ -65,6 +79,7 @@ let empty =
   ; typ_vars= empty_ident_table
   ; types= empty_ident_table
   ; fields= empty_ident_table
+  ; constructors= empty_ident_table
   ; match_instances= []
   ; vars_size= 0
   ; depth= 0 }
@@ -82,7 +97,7 @@ let add_type_var ~user ~name var env =
 
 let find_type_var name {typ_vars; _} = Map.find typ_vars name.txt
 
-let register_type name typ_decl env =
+let rec register_type name typ_decl env =
   let env =
     {env with types= Map.update env.types name.txt ~f:(fun _ -> typ_decl)}
   in
@@ -93,6 +108,21 @@ let register_type name typ_decl env =
         fields=
           List.foldi fields ~init:env.fields ~f:(fun i env {field_ident; _} ->
               Map.update env field_ident.txt ~f:(fun _ -> (typ, i)) ) }
+  | Variant ctors ->
+      let typ = Type.mk_constr' ~loc:name.loc ~decl:typ_decl name in
+      List.foldi ctors ~init:env
+        ~f:(fun i env {constr_decl_ident= ident; constr_decl_args= args; _} ->
+          let env =
+            { env with
+              constructors=
+                Map.update env.constructors ident.txt ~f:(fun _ -> (typ, i)) }
+          in
+          match args with
+          | Constr_tuple _ -> env
+          | Constr_record fields ->
+              register_type ident
+                (TypeDecl.mk ~loc:ident.loc (VariantRecord fields))
+                env )
   | _ -> env
 
 let find_type name {types; _} = Map.find types name.txt
@@ -101,13 +131,36 @@ let find_record_type name {fields; _} =
   Option.map ~f:fst (Map.find fields name.txt)
 
 let find_field_type name {fields; _} =
-  match Map.find fields name.txt with
-  | None -> None
-  | Some (typ, i) -> (
-    match typ.type_desc with
-    | Tconstr {constr_type_decl= {type_decl_desc= Record fields; _}; _} ->
-        Some (List.nth_exn fields i).field_type
-    | _ -> None )
+  let%bind typ, i = Map.find fields name.txt in
+  match typ.type_desc with
+  | Tconstr
+      { constr_type_decl=
+          {type_decl_desc= Record fields | VariantRecord fields; _}; _ } ->
+      let%map {field_type; _} = List.nth fields i in
+      field_type
+  | _ -> None
+
+let find_constructor_types name env =
+  let%bind typ, i = Map.find env.constructors name.txt in
+  match typ.type_desc with
+  | Tconstr {constr_type_decl= {type_decl_desc= Variant ctors; _}; _} ->
+      let%bind { constr_decl_return= typ_ret
+               ; constr_decl_loc= loc
+               ; constr_decl_ident= ident
+               ; constr_decl_args= args } =
+        List.nth ctors i
+      in
+      let return_type = Option.value ~default:typ typ_ret in
+      let%map argument_type =
+        match args with
+        | Constr_tuple [x] -> Some x
+        | Constr_tuple types -> Some (Type.mk ~loc (Ttuple types))
+        | Constr_record _ ->
+            let%map record_typ = find_type ident env in
+            Type.mk_constr' ~loc ~decl:record_typ ident
+      in
+      (argument_type, return_type)
+  | _ -> None
 
 let push_match_instances instances env =
   {env with match_instances= instances :: env.match_instances}
