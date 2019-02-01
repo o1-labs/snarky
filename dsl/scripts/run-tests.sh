6e8d775d2fb886232538943d413d96eeb31ceaf0
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-01 19:13:59 +0000

    Add a script for updating test output

--- dsl/scripts/run-tests.sh
+++ dsl/scripts/run-tests.sh
@@ -5,42 +5,68 @@ run_dune() {
 }
 
 declare -i passes
+passes=0
 declare -i fails
+fails=0
+declare -i update_output
+update_output=0
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 check_diff() {
-    diff -N "tests/$1" "tests/out/$1"
+    diff -Nq "tests/$1" "tests/out/$1" > /dev/null
     local STATUS=$?
     if [ $STATUS -ne 0 ]; then
-      echo -e "${RED}FAILED${NC}: $1 does not match expected output"
       fails=fails+1
+      if [[ "$update_output" -eq 0 ]]; then
+        echo -e "${RED}FAILED${NC}: $1 does not match expected output"
+        diff -N "tests/$1" "tests/out/$1"
+      else
+        echo "Updating tests/$1"
+        cp "tests/out/$1" "tests/$1"
+      fi
     else
-      echo -e "${GREEN}PASSED${NC}: $1 matches expected output"
-      passes=passes+1
+      if [[ "$update_output" -eq 0 ]]; then
+        echo -e "${GREEN}PASSED${NC}: $1 matches expected output"
+        passes=passes+1
+      fi
     fi
 }
 
 run_test() {
-  run_dune exec dsl/meja.exe -- --ml "tests/out/$1.ml" --ocaml-env "tests/out/$FILENAME.ocaml-env" --stderr "tests/out/$1.stderr" "tests/$1.meja"
+  run_dune exec dsl/meja.exe -- --ml "tests/out/$1.ml" --ocaml-env "tests/out/$FILENAME.ocaml-env" --stderr "tests/out/$1.stderr" "tests/$1.meja" 2> /dev/null
   if [ $? -ne 0 ]; then
     if [ -e "tests/$1.fail" ]; then
-      echo -e "${GREEN}PASSED${NC}: Got expected failure building from $1.mega"
-      passes=passes+1
+      if [[ "$update_output" -eq 0 ]]; then
+        echo -e "${GREEN}PASSED${NC}: Got expected failure building from $1.meja"
+        passes=passes+1
+      fi
     else
-      echo -e "${RED}FAILED${NC}: Building from $1.mega"
-      cat "tests/out/$1.stderr"
       fails=fails+1
+      if [[ "$update_output" -eq 0 ]]; then
+        echo -e "${RED}FAILED${NC}: Building from $1.meja"
+        cat "tests/out/$1.stderr"
+      else
+        echo "Adding tests/$1.fail: Failed to build $1.meja"
+        touch "tests/$1.fail"
+      fi
     fi
   else
     if [ -e "tests/$1.fail" ]; then
-      echo -e "${RED}FAILED${NC}: Expected failure building from $1.mega; got success"
       fails=fails+1
+      if [[ "$update_output" -eq 0 ]]; then
+        echo -e "${RED}FAILED${NC}: Expected failure building from $1.meja; got success"
+      else
+        echo "Removing tests/$1.fail: Successfully built $1.meja"
+        rm "tests/$1.fail"
+      fi
     else
-      echo -e "${GREEN}PASSED${NC}: Building from $1.mega"
-      passes=passes+1
+      if [[ "$update_output" -eq 0 ]]; then
+        echo -e "${GREEN}PASSED${NC}: Building from $1.meja"
+        passes=passes+1
+      fi
     fi
     ocamlformat tests/out/$1.ml > tests/out/$1.ml.reformat
     mv tests/out/$1.ml.reformat tests/out/$1.ml
@@ -58,11 +84,19 @@ run_tests() {
   done
   declare -i total
   total=passes+fails
-  if [[ "$fails" -ne 0 ]]; then
+  if [[ "$update_output" -ne 0 ]]; then
+    echo -e "${GREEN}UP-TO-DATE${NC} $fails files changed"
+    return 0
+  elif [[ "$fails" -ne 0 ]]; then
     echo -e "${RED}FAILED${NC} $fails/$total"
     return 1
   else
     echo -e "${GREEN}PASSED${NC} $passes/$total"
     return 0
   fi
+}
+
+update_test_output() {
+  update_output=1
+  run_tests
 }
