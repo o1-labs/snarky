dd74fb9b44ebc64e96a061caef9ae11be4341a6c
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-04 19:55:21 +0000

    Reformat

--- meja/parsetypes.ml
+++ meja/parsetypes.ml
@@ -5,11 +5,11 @@ type pattern = PVariable of str
 type typ = TAny | TVariable of str
 
 type expression =
-| Apply of expression * expression
-| Variable of str
-| Int of int
-| Fun of (pattern * typ option) list * typ option * expression
-| Seq of expression * expression
-| Let of pattern * expression * expression
+  | Apply of expression * expression
+  | Variable of str
+  | Int of int
+  | Fun of (pattern * typ option) list * typ option * expression
+  | Seq of expression * expression
+  | Let of pattern * expression * expression
 
 type statement = Value of pattern * expression
