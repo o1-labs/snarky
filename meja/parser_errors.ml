ace195aa3179408c5c61ce43b5865b4dd40c3060
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-07 15:19:51 +0000

    Add nicer parser errors

---
+++ meja/parser_errors.ml
@@ -0,0 +1,23 @@
+type error =
+  | Fun_no_fat_arrow
+  | Missing_semi
+  | Unexpected_character of string
+
+exception Error of Location.t * error
+
+open Format
+
+let pp_typ ppf typ = Pprintast.core_type ppf (To_ocaml.of_type_expr typ)
+
+let report_error ppf = function
+  | Fun_no_fat_arrow ->
+    fprintf ppf "Expected => before {@."
+  | Missing_semi ->
+    fprintf ppf "Missing semicolon."
+  | Unexpected_character x ->
+    fprintf ppf "Unexpected character '%s'" x
+
+let () =
+  Location.register_error_of_exn (function
+    | Error (loc, err) -> Some (Location.error_of_printer loc report_error err)
+    | _ -> None )
