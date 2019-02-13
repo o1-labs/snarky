02337e4cb0df0fb5ac9bf7d06ca71e52e17cfe2d
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-13 18:29:34 +0000

    Add some tests for record patterns

--- meja/tests/match.ml
+++ meja/tests/match.ml
@@ -1,3 +1,13 @@
 let x (y : int) (z : bool) = match y with i -> z
 
 let y (f : int -> int -> int -> _) = match (1, 2, 3) with i, j, k -> f i j k
+
+type t = {a: int; b: bool}
+
+type u = {f: int -> int}
+
+let z x {f; _} = match x with {a= x; b; _} -> f x
+
+let a (x : t) {f; _} = match x with {a= x; b; _} -> f x
+
+let b = match {f= (fun x -> 12)} with {f; _} -> f
