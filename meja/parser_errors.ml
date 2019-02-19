8a4f09c626296d395caff34819326ba5b4d49daf
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-19 19:58:42 +0000

    Add parsing for operator names

--- meja/parser_errors.ml
+++ meja/parser_errors.ml
@@ -1,4 +1,8 @@
-type error = Fun_no_fat_arrow | Missing_semi | Unexpected_character of string
+type error =
+  | Fun_no_fat_arrow
+  | Missing_semi
+  | Unexpected_character of string
+  | Expecting of string
 
 exception Error of Location.t * error
 
@@ -10,6 +14,7 @@ let report_error ppf = function
   | Fun_no_fat_arrow -> fprintf ppf "Expected => before {@."
   | Missing_semi -> fprintf ppf "Missing semicolon."
   | Unexpected_character x -> fprintf ppf "Unexpected character '%s'" x
+  | Expecting desc -> fprintf ppf "Syntax error: %s expected" desc
 
 let () =
   Location.register_error_of_exn (function
