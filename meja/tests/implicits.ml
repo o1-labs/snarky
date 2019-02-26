0b60bf5f9784449de2c13cf03f4444322fa0580b
mrmr1993 <mr_1993@hotmail.co.uk>
2019-02-26 13:57:05 +0000

    Extend implicit example, rename

--- meja/tests/implicits.ml
+++ meja/tests/implicits.ml
@@ -1,8 +1,8 @@
-let x __implicit2__ (x : 'a) =
+let f __implicit2__ (x : 'a) =
   let f __implicit1__ x = (testing_show __implicit1__) x in
   (f __implicit2__) x
 
-let y __implicit3__ (x : 'a) (y : 'a) =
+let g __implicit3__ (x : 'a) (y : 'a) =
   let a = (testing_show __implicit3__) x in
   let b = (testing_show __implicit3__) y in
   let c = (testing_show __implicit5__) 15 in
@@ -10,3 +10,6 @@ let y __implicit3__ (x : 'a) (y : 'a) =
   let e = (testing_show __implicit7__) true in
   let f = (testing_show __implicit7__) false in
   (a, b, c, d, e, f)
+
+let h (x : int) (y : bool) (z : float) =
+  ((g __implicit5__) x x, (g __implicit7__) y y, (g __implicit11__) z z)
