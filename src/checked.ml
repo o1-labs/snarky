a134784484e87beabdeddb2cf82a3bcec4efae29
Matthew Ryan <MR_1993@hotmail.co.uk>
2019-03-26 13:19:36 +0000

    Inline Run_state.t in Checked.t (#153)
    * Inline Run_state.t in Checked.t, remove the type parameter
    
    This also changes the relationship between the two: now Run_state.t is
    assumed to have the same prover_state type as the checked computation
    that wraps it.
    
    For now, we just work around this by updating the prover_state to unit
    as we enter an imperative computation; later, this will be useful when
    we switch over to non-unit prover state for the imperative API.
    
    * I accidentally a line

--- src/checked.ml
+++ src/checked.ml
@@ -1,15 +1,15 @@
 open Core_kernel
 open Types.Checked
 
-type ('a, 's, 'field, 'r) t = ('a, 's, 'field, 'r) Types.Checked.t
+type ('a, 's, 'field) t = ('a, 's, 'field) Types.Checked.t
 
 module T0 = struct
   let return x = Pure x
 
   let as_prover x = As_prover (x, return ())
 
-  let rec map : type s a b field r.
-      (a, s, field, r) t -> f:(a -> b) -> (b, s, field, r) t =
+  let rec map : type s a b field.
+      (a, s, field) t -> f:(a -> b) -> (b, s, field) t =
    fun t ~f ->
     match t with
     | Pure x -> Pure (f x)
@@ -26,8 +26,8 @@ module T0 = struct
 
   let map = `Custom map
 
-  let rec bind : type s a b field r.
-      (a, s, field, r) t -> f:(a -> (b, s, field, r) t) -> (b, s, field, r) t =
+  let rec bind : type s a b field.
+      (a, s, field) t -> f:(a -> (b, s, field) t) -> (b, s, field) t =
    fun t ~f ->
     match t with
     | Pure x -> f x
@@ -80,7 +80,7 @@ end
 module T = struct
   include T0
 
-  let request_witness (typ : ('var, 'value, 'field, 'r) Types.Typ.t)
+  let request_witness (typ : ('var, 'value, 'field) Types.Typ.t)
       (r : ('value Request.t, 'field, 's) As_prover0.t) =
     Exists (typ, Request r, fun h -> return (Handle.var h))
 
