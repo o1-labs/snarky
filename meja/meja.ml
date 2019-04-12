7b48d2c29bc19b5be12fd9a97fdc074a1d109c08
mrmr1993 <mr_1993@hotmail.co.uk>
2019-04-12 17:18:11 +0100

    Make output of cmi_to_meja less horrible

--- meja/meja.ml
+++ meja/meja.ml
@@ -116,12 +116,14 @@ let main =
     Format.sprintf "Usage:@.@[%s [options] file@]@.@.OPTIONS:"
       (Filename.basename Sys.executable_name)
   in
-  Arg.parse arg_spec (fun filename ->
-    match !file with
-    | Some _ ->
-        Arg.usage arg_spec usage_text ;
-        exit 1
-    | None -> file := Some filename) usage_text ;
+  Arg.parse arg_spec
+    (fun filename ->
+      match !file with
+      | Some _ ->
+          Arg.usage arg_spec usage_text ;
+          exit 1
+      | None -> file := Some filename )
+    usage_text ;
   let env = Envi.Core.env in
   try
     let env =
@@ -194,13 +196,16 @@ let main =
           Envi.open_namespace_scope scope env )
     in
     let meji_files = List.rev !meji_files in
-    let env = List.fold ~init:env meji_files ~f:(fun env file ->
-      let parse_ast = read_file (Parser_impl.interface Lexer_impl.token) file in
-      let env = Envi.open_module env in
-      let env = Typechecker.check_signature env parse_ast in
-      let m, env = Envi.pop_module ~loc:Location.none env in
-      let name = Location.(mkloc (Loader.modname_of_filename file) none) in
-      Envi.add_module name m env )
+    let env =
+      List.fold ~init:env meji_files ~f:(fun env file ->
+          let parse_ast =
+            read_file (Parser_impl.interface Lexer_impl.token) file
+          in
+          let env = Envi.open_module env in
+          let env = Typechecker.check_signature env parse_ast in
+          let m, env = Envi.pop_module ~loc:Location.none env in
+          let name = Location.(mkloc (Loader.modname_of_filename file) none) in
+          Envi.add_module name m env )
     in
     let file =
       match !file with
@@ -210,7 +215,9 @@ let main =
           Arg.usage arg_spec usage_text ;
           exit 1
     in
-    let parse_ast = read_file (Parser_impl.implementation Lexer_impl.token) file in
+    let parse_ast =
+      read_file (Parser_impl.implementation Lexer_impl.token) file
+    in
     let _env, ast = Typechecker.check parse_ast env in
     let ast =
       if !snarky_preamble then add_preamble !impl_mod !curve !proofs ast
--- meja/src/pprint.ml
+++ meja/src/pprint.ml
@@ -12,9 +12,9 @@ let rec type_desc ?(bracket = false) fmt = function
   | Tarrow (typ1, typ2, implicitness) ->
       if bracket then fprintf fmt "(" ;
       ( match implicitness with
-      | Explicit -> fprintf fmt "%a" type_expr_b typ1
-      | Implicit -> fprintf fmt "{%a}" type_expr typ1 ) ;
-      fprintf fmt "@ ->@ %a" type_expr typ2 ;
+      | Explicit -> fprintf fmt "@[%a" type_expr_b typ1
+      | Implicit -> fprintf fmt "@[{%a}" type_expr typ1 ) ;
+      fprintf fmt "@ ->@ %a@]" type_expr typ2 ;
       if bracket then fprintf fmt ")"
   | Tctor v -> variant fmt v
   | Tpoly (vars, typ) ->
@@ -24,7 +24,7 @@ let rec type_desc ?(bracket = false) fmt = function
       if bracket then fprintf fmt ")"
 
 and tuple fmt typs =
-  fprintf fmt "(%a)" (pp_print_list ~pp_sep:comma_sep type_expr) typs
+  fprintf fmt "(@[%a@])" (pp_print_list ~pp_sep:comma_sep type_expr) typs
 
 and type_expr fmt typ = type_desc fmt typ.type_desc
 
@@ -33,10 +33,10 @@ and type_expr_b fmt typ = type_desc ~bracket:true fmt typ.type_desc
 and variant fmt v =
   match v.var_params with
   | [] -> Longident.pp fmt v.var_ident.txt
-  | _ -> fprintf fmt "%a(%a)" Longident.pp v.var_ident.txt tuple v.var_params
+  | _ -> fprintf fmt "%a%a" Longident.pp v.var_ident.txt tuple v.var_params
 
 let field_decl fmt decl =
-  fprintf fmt "%s:@ %a" decl.fld_ident.txt type_expr decl.fld_type
+  fprintf fmt "%s:@ @[%a@]" decl.fld_ident.txt type_expr decl.fld_type
 
 let ctor_args fmt = function
   | Ctor_tuple [] -> ()
@@ -47,7 +47,7 @@ let ctor_args fmt = function
 let ctor_decl fmt decl =
   fprintf fmt "%s%a" decl.ctor_ident.txt ctor_args decl.ctor_args ;
   match decl.ctor_ret with
-  | Some typ -> fprintf fmt "@ :@ %a" type_expr typ
+  | Some typ -> fprintf fmt "@ :@ @[%a@]" type_expr typ
   | None -> ()
 
 let type_decl_desc fmt = function
@@ -78,16 +78,16 @@ let type_decl fmt decl =
   type_decl_desc fmt decl.tdec_desc
 
 let rec signature_desc fmt = function
-  | SValue (name, typ) -> fprintf fmt "let@ %s@ :@ %a;@.@." name.txt type_expr typ
+  | SValue (name, typ) -> fprintf fmt "@[let@ %s@ :@ @[%a;@]@]@;@;" name.txt type_expr typ
   | SInstance (name, typ) ->
-      fprintf fmt "instance@ %s@ : %a;@.@." name.txt type_expr typ
-  | STypeDecl decl -> type_decl fmt decl
+      fprintf fmt "@[instance@ %s@ :@ @[%a@];@]@;@;" name.txt type_expr typ
+  | STypeDecl decl -> fprintf fmt "@[%a;@]@;@;" type_decl decl
   | SModule (name, msig) ->
       let prefix fmt = fprintf fmt ":@ " in
-      fprintf fmt "module@ %s@ %a;@.@." name.txt (module_sig ~prefix) msig
+      fprintf fmt "@[module@ %s@ %a;@]@;@;" name.txt (module_sig ~prefix) msig
   | SModType (name, msig) ->
       let prefix fmt = fprintf fmt "=@ " in
-      fprintf fmt "module type@ %s@ %a;@.@." name.txt (module_sig ~prefix) msig
+      fprintf fmt "@[module type@ %s@ %a;@]@;@;" name.txt (module_sig ~prefix) msig
 
 and signature_item fmt sigi = signature_desc fmt sigi.sig_desc
 
@@ -95,7 +95,7 @@ and signature fmt sigs = List.iter (signature_item fmt) sigs
 
 and module_sig_desc ~prefix fmt = function
   | Signature msig -> prefix fmt;
-    fprintf fmt "{%a}" signature msig
+    fprintf fmt "{@;@[<v2>%a@]}@;" signature msig
   | SigName name -> prefix fmt; Longident.pp fmt name.txt
   | SigAbstract -> ()
   | SigFunctor (name, f, m) ->
