f37cd5e10d5d3d5855d719b39f05c345bab6adc3
mrmr1993 <mr_1993@hotmail.co.uk>
2019-04-15 13:27:48 +0100

    Fix cmi/cmti search

--- meja/loader.ml
+++ meja/loader.ml
@@ -20,7 +20,7 @@ let load_directory env dirname =
   let files = try Sys.readdir dirname with Sys_error _ -> [||] in
   Array.iter files ~f:(fun file ->
       match Filename.split_extension file with
-      | _, Some (".cmi" | ".cmti") ->
+      | _, Some ("cmi" | "cmti") ->
           let filename = Filename.concat dirname file in
           let module_name = modname_of_filename file in
           Envi.register_external_module module_name (Envi.Deferred filename)
