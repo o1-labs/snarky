237715c9684b6e736eeffb647ef78a162055d1fa
mrmr1993 <mr_1993@hotmail.co.uk>
2019-04-23 18:21:02 +0100

    Log more information for unsatisfied constraints

--- src/snark0.ml
+++ src/snark0.ml
@@ -96,10 +96,44 @@ module Runner = struct
         failwith
           "Cannot add a constraint as the prover: the verifier's constraint \
            system will not match." ;
-      if s.eval_constraints && not (Constraint.eval c (get_value s)) then
-        failwithf "Constraint unsatisfied:\n%s\n%s\n" (Constraint.annotation c)
+      ( if s.eval_constraints && not (Constraint.eval c (get_value s)) then
+        let data =
+          String.concat ~sep:"\n"
+            (List.map c ~f:(fun {basic; _} ->
+                 match basic with
+                 | Boolean var ->
+                     Format.(
+                       asprintf "Boolean %s"
+                         (Field.to_string (get_value s var)))
+                 | Equal (var1, var2) ->
+                     Format.(
+                       asprintf "Equal %s %s"
+                         (Field.to_string (get_value s var1))
+                         (Field.to_string (get_value s var2)))
+                 | Square (var1, var2) ->
+                     Format.(
+                       asprintf "Square %s %s"
+                         (Field.to_string (get_value s var1))
+                         (Field.to_string (get_value s var2)))
+                 | R1CS (var1, var2, var3) ->
+                     Format.(
+                       asprintf "R1CS %s %s %s"
+                         (Field.to_string (get_value s var1))
+                         (Field.to_string (get_value s var2))
+                         (Field.to_string (get_value s var3))) ))
+        in
+        failwithf
+          "Constraint unsatisfied (unreduced):\n\
+           %s\n\
+           %s\n\n\
+           Constraint:\n\
+           %s\n\
+           Data:\n\
+           %s"
+          (Constraint.annotation c)
           (Constraint.stack_to_string s.stack)
-          () ;
+          (Sexp.to_string (Constraint.sexp_of_t c))
+          data () ) ;
       Option.iter s.system ~f:(fun system ->
           Constraint.add ~stack:s.stack c system ) ;
       (s, ())
@@ -190,7 +224,7 @@ module Runner = struct
 
     let dummy_vector = Field.Vector.create ()
 
-    let fake_state next_auxiliary =
+    let fake_state next_auxiliary stack =
       { system= None
       ; input= dummy_vector
       ; aux= dummy_vector
@@ -198,18 +232,21 @@ module Runner = struct
       ; num_inputs= 0
       ; next_auxiliary
       ; prover_state= None
-      ; stack= []
+      ; stack
       ; handler= Request.Handler.fail
       ; is_running= true
       ; as_prover= ref false
       ; run_special= None }
 
     let rec flatten_as_prover : type a s.
-        int ref -> (a, s, Field.t) t -> (s run_state -> s run_state) * a =
-     fun next_auxiliary t ->
+           int ref
+        -> string list
+        -> (a, s, Field.t) t
+        -> (s run_state -> s run_state) * a =
+     fun next_auxiliary stack t ->
       match t with
       | As_prover (x, k) ->
-          let f, a = flatten_as_prover next_auxiliary k in
+          let f, a = flatten_as_prover next_auxiliary stack k in
           ( (fun s ->
               let s', (_ : unit option) = run_as_prover (Some x) s in
               f s' )
@@ -217,26 +254,68 @@ module Runner = struct
       | Pure x ->
           (Fn.id, x)
       | Direct (d, k) ->
-          let _, y = d (fake_state next_auxiliary) in
-          let f, a = flatten_as_prover next_auxiliary (k y) in
+          let _, y = d (fake_state next_auxiliary stack) in
+          let f, a = flatten_as_prover next_auxiliary stack (k y) in
           ( (fun s ->
               let {prover_state; _} = s in
               let s, _y = d s in
               f (set_prover_state prover_state s) )
           , a )
       | Reduced (t, d, _res, k) ->
-          let f, y = flatten_as_prover next_auxiliary t in
-          let g, a = flatten_as_prover next_auxiliary (k y) in
+          let f, y = flatten_as_prover next_auxiliary stack t in
+          let g, a = flatten_as_prover next_auxiliary stack (k y) in
           ((fun s -> g (f s)), a)
       | With_label (lab, t, k) ->
-          let f, y = flatten_as_prover next_auxiliary t in
-          let g, a = flatten_as_prover next_auxiliary (k y) in
+          let f, y = flatten_as_prover next_auxiliary (lab :: stack) t in
+          let g, a = flatten_as_prover next_auxiliary stack (k y) in
           ((fun s -> g (f s)), a)
       | Add_constraint (c, t) ->
-          flatten_as_prover next_auxiliary t
+          let f, y = flatten_as_prover next_auxiliary stack t in
+          ( (fun s ->
+              ( if s.eval_constraints && not (Constraint.eval c (get_value s))
+              then
+                let data =
+                  String.concat ~sep:"\n"
+                    (List.map c ~f:(fun {basic; _} ->
+                         match basic with
+                         | Boolean var ->
+                             Format.(
+                               asprintf "Boolean %s"
+                                 (Field.to_string (get_value s var)))
+                         | Equal (var1, var2) ->
+                             Format.(
+                               asprintf "Equal %s %s"
+                                 (Field.to_string (get_value s var1))
+                                 (Field.to_string (get_value s var2)))
+                         | Square (var1, var2) ->
+                             Format.(
+                               asprintf "Square %s %s"
+                                 (Field.to_string (get_value s var1))
+                                 (Field.to_string (get_value s var2)))
+                         | R1CS (var1, var2, var3) ->
+                             Format.(
+                               asprintf "R1CS %s %s %s"
+                                 (Field.to_string (get_value s var1))
+                                 (Field.to_string (get_value s var2))
+                                 (Field.to_string (get_value s var3))) ))
+                in
+                failwithf
+                  "Constraint unsatisfied:\n\
+                   %s\n\
+                   %s\n\n\
+                   Constraint:\n\
+                   %s\n\
+                   Data:\n\
+                   %s"
+                  (Constraint.annotation c)
+                  (Constraint.stack_to_string stack)
+                  (Sexp.to_string (Constraint.sexp_of_t c))
+                  data () ) ;
+              f s )
+          , y )
       | With_state (p, and_then, t_sub, k) ->
-          let f_sub, y = flatten_as_prover next_auxiliary t_sub in
-          let f, a = flatten_as_prover next_auxiliary (k y) in
+          let f_sub, y = flatten_as_prover next_auxiliary stack t_sub in
+          let f, a = flatten_as_prover next_auxiliary stack (k y) in
           ( (fun s ->
               let s, s_sub = run_as_prover (Some p) s in
               let s_sub = f_sub (set_prover_state s_sub s) in
@@ -246,16 +325,16 @@ module Runner = struct
               f s )
           , a )
       | With_handler (h, t, k) ->
-          let f, y = flatten_as_prover next_auxiliary t in
-          let g, a = flatten_as_prover next_auxiliary (k y) in
+          let f, y = flatten_as_prover next_auxiliary stack t in
+          let g, a = flatten_as_prover next_auxiliary stack (k y) in
           ( (fun s ->
               let {handler; _} = s in
               let s' = f {s with handler= Request.Handler.push handler h} in
               g {s' with handler} )
           , a )
       | Clear_handler (t, k) ->
-          let f, y = flatten_as_prover next_auxiliary t in
-          let g, a = flatten_as_prover next_auxiliary (k y) in
+          let f, y = flatten_as_prover next_auxiliary stack t in
+          let g, a = flatten_as_prover next_auxiliary stack (k y) in
           ( (fun s ->
               let {handler; _} = s in
               let s' = f {s with handler= Request.Handler.fail} in
@@ -263,11 +342,12 @@ module Runner = struct
           , a )
       | Exists ({store; alloc; check; _}, p, k) ->
           let var =
-            Typ_monads.Alloc.run alloc (alloc_var (fake_state next_auxiliary))
+            Typ_monads.Alloc.run alloc
+              (alloc_var (fake_state next_auxiliary stack))
           in
-          let f, () = flatten_as_prover next_auxiliary (check var) in
+          let f, () = flatten_as_prover next_auxiliary stack (check var) in
           let handle = {Handle.var; value= None} in
-          let g, a = flatten_as_prover next_auxiliary (k handle) in
+          let g, a = flatten_as_prover next_auxiliary stack (k handle) in
           ( (fun s ->
               let old = !(s.as_prover) in
               s.as_prover := true ;
@@ -285,11 +365,11 @@ module Runner = struct
               g (set_prover_state (Some ps) s) )
           , a )
       | Next_auxiliary k ->
-          flatten_as_prover next_auxiliary (k !next_auxiliary)
+          flatten_as_prover next_auxiliary stack (k !next_auxiliary)
 
     let reduce_to_prover (type a s) next_auxiliary (t : (a, s, Field.t) t) :
         (a, s, Field.t) t =
-      let f, a = flatten_as_prover next_auxiliary t in
+      let f, a = flatten_as_prover next_auxiliary [] t in
       Reduced (t, f, a, return)
 
     module State = struct
