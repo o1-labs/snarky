d013e8afd170d4de43ac05bc34b9767bb9848994
mrmr1993 <mr_1993@hotmail.co.uk>
2019-04-24 00:47:39 +0100

    Revert "Backtrack to Core v11"
    This reverts commit 680b7514d62e0a672c55fc4561da7e8fa4f391e4.

--- fold_lib/fold.ml
+++ fold_lib/fold.ml
@@ -47,7 +47,7 @@ let of_array (xs : 'a array) : 'a t =
   {fold= (fun ~init ~f -> Array.fold xs ~init ~f)}
 
 let%test_unit "fold-to-list" =
-  Quickcheck.test (Quickcheck.Generator.list Int.gen)
+  Quickcheck.test (Quickcheck.Generator.list Int.quickcheck_generator)
     ~f:(fun xs -> assert (xs = to_list (of_list xs)))
 
 let sexp_of_t f t = List.sexp_of_t f (to_list t)
@@ -82,7 +82,7 @@ let group3 ~default (t : 'a t) : ('a * 'a * 'a) t =
             assert false ) }
 
 let%test_unit "group3" =
-  Quickcheck.test (Quickcheck.Generator.list Int.gen)
+  Quickcheck.test (Quickcheck.Generator.list Int.quickcheck_generator)
     ~f:(fun xs ->
       let default = 0 in
       let n = List.length xs in
