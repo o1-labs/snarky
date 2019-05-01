c1de2e957021f8497a726b6a2d94a11866ee269f
mrmr1993 <mr_1993@hotmail.co.uk>
2019-05-01 14:16:03 +0100

    Use monadic version to implement imperative for Enumerable

--- src/enumerable.ml
+++ src/enumerable.ml
@@ -60,43 +60,14 @@ module Run = struct
       end) =
   struct
     open Impl
+    include Make (Impl.Internal_Basic) (M)
 
-    (* TODO: Make this throw when the elt is too big *)
-    let field_to_int x = int_of_bits (List.take (Field.Constant.unpack x) 62)
+    let assert_equal x y = run_checked (assert_equal x y)
 
-    open M
+    let var_to_bits x = run_checked (var_to_bits x)
 
-    let bit_length =
-      let n = Int.ceil_log2 (M.max + 1) in
-      assert (n < Field.size_in_bits) ;
-      n
+    let if_ x ~then_ ~else_ = run_checked (if_ x ~then_ ~else_)
 
-    type var = Field.t
-
-    let to_field t = Field.Constant.of_int (to_enum t)
-
-    let of_field x = Option.value_exn (of_enum (field_to_int x))
-
-    let assert_equal x y = Field.Assert.equal x y
-
-    let typ : (var, t) Typ.t =
-      let check =
-        if M.max = 1 then fun x ->
-          make_checked @@ fun () -> assert_ (Constraint.boolean x)
-        else fun x ->
-          make_checked
-          @@ fun () -> Field.Assert.lte ~bit_length x (Field.of_int M.max)
-      in
-      {(Typ.transport Field.typ ~there:to_field ~back:of_field) with check}
-
-    let var_to_bits : var -> Boolean.var list = Field.unpack ~length:bit_length
-
-    let to_bits t = int_to_bits ~length:bit_length (to_enum t)
-
-    let if_ b ~(then_ : var) ~(else_ : var) = Field.if_ b ~then_ ~else_
-
-    let var t : var = Field.constant (to_field t)
-
-    let ( = ) = Field.equal
+    let (=) x y = run_checked (x = y)
   end
 end
