e162852379c686527b2c3b9abd6b0af3bc8994d7
mrmr1993 <mr_1993@hotmail.co.uk>
2019-05-03 14:55:40 +0100

    Add printing for Type0.type_expr, share common functions in Ast_print

--- meja/src/pprint.ml
+++ meja/src/pprint.ml
@@ -1,24 +1,7 @@
 open Ast_types
 open Parsetypes
 open Format
-
-let comma_sep fmt () = fprintf fmt ",@ "
-
-let bar_sep fmt () = fprintf fmt "@ | "
-
-let arg_label fmt = function
-  | Asttypes.Nolabel ->
-      ()
-  | Labelled label ->
-      fprintf fmt "@[<hv2>%s:@," label
-  | Optional label ->
-      fprintf fmt "@[<hv2>?%s:@," label
-
-let arg_label_box_end fmt = function
-  | Asttypes.Nolabel ->
-      ()
-  | _ ->
-      fprintf fmt "@]"
+open Ast_print
 
 let rec type_desc ?(bracket = false) fmt = function
   | Tvar (None, _, _) ->
