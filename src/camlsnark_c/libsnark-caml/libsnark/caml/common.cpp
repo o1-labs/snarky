84046270f568f99f5f0b7a22c157a814dbecd3d9
mrmr1993 <mr_1993@hotmail.co.uk>
2019-05-09 20:06:43 +0100

    Implement JSON logging format in C++ for libsnark

--- src/camlsnark_c/libsnark-caml/libsnark/caml/common.cpp
+++ src/camlsnark_c/libsnark-caml/libsnark/caml/common.cpp
@@ -7,79 +7,124 @@
 #include <libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp>
 #include <libff/common/profiling.hpp>
 #include <stdarg.h>
+#include <time.h>
 
-int (*snarky_printf_deferred)(const char* format, va_list args) = &vprintf;
+int normal_printf(const char* filename,
+                  int line,
+                  FILE* file,
+                  const char* format,
+                  va_list args) {
+    return vfprintf(file, format, args);
+}
 
-int snarky_printf(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    int ret = (*snarky_printf_deferred)(format, args);
-    va_end(args);
+int json_printf(const char* filename,
+                int line,
+                FILE* file,
+                const char* format,
+                va_list args) {
+    time_t now;
+    struct tm* now_utc;
+
+    time(&now);
+    now_utc = gmtime(&now);
+    fprintf(file,
+            "{\"timestamp\":\"%d-%02d-%02d %02d-%02d:%02d:%06dZ",
+            now_utc->tm_year + 1900,
+            now_utc->tm_mon,
+            now_utc->tm_mday,
+            now_utc->tm_hour,
+            now_utc->tm_min,
+            now_utc->tm_sec,
+            0);
+    fprintf (file, "\",\"level\":\"%s\"", "DEBUG");
+    fprintf (file,
+             ",\"source\":{\"module\":\"%s\",\"location\":\"File \"%s\", line %d, characters 0-1\"},\"message\":\"",
+             "Snarky__Libsnark",
+             filename,
+             line);
+    const int size = 1024;
+    char inbuf[size];
+    char outbuf[size];
+    int ret = vsnprintf(inbuf, 1024, format, args);
+    int i;
+    int j;
+    for (i = j = 0; i < size - 1; i++, j++) {
+        if (inbuf[j] == '\0') {
+            outbuf[i] = '\0';
+            break;
+        } else if (inbuf[j] == '\n') {
+            outbuf[i] = '\\';
+            i++;
+            outbuf[i] = 'n';
+        } else if (inbuf[j] == '\\') {
+            outbuf[i] = '\\';
+            i++;
+            outbuf[i] = '\\';
+        } else {
+            outbuf[i] = inbuf[j];
+        }
+    }
+    if (outbuf[i] != '\0') {
+        outbuf[i+1] = '\0';
+    }
+    fputs(outbuf, file);
+    fputs("\",\"metadata\":{}}\n", file);
     return ret;
 }
 
-int no_print(const char* format, va_list args) {
-    return 0;
-}
+int (*snarky_printf_deferred)(const char* filename,
+                              int line,
+                              FILE* file,
+                              const char* format,
+                              va_list args) = &normal_printf;
 
-FILE *snarky_print_dest = NULL;
+FILE *snarky_print_dest = stdout;
 
 int close_snarky_print_dest() {
     if (snarky_print_dest) {
-        return fclose(snarky_print_dest);
+        int ret = fclose(snarky_print_dest);
+        snarky_print_dest = NULL;
+        return ret;
     } else {
         return 0;
     }
 }
 
-int file_print(const char* format, va_list args) {
-    return vfprintf(snarky_print_dest, format, args);
-}
-
-int (*snarky_print_func)(char*) = NULL;
-
-int snarky_print_to_func(const char* format, va_list args) {
-    char output[1024];
-    int true_size = vsnprintf(output, 1024, format, args);
-    if (true_size >= 0) {
-        return (*snarky_print_func)(output);
-    } else {
-        return (-true_size);
+int snarky_printf(const char* filename, int line, const char* format, ...) {
+    if (snarky_print_dest) {
+        va_list args;
+        va_start(args, format);
+        int ret = (*snarky_printf_deferred)(filename, line, snarky_print_dest, format, args);
+        va_end(args);
+        return ret;
+    }
+    else {
+        return 0;
     }
 }
 
 extern "C" {
 
 void camlsnark_set_printing_off() {
-    snarky_printf_deferred = &no_print;
     close_snarky_print_dest();
 }
 
 void camlsnark_set_printing_stdout() {
-    snarky_printf_deferred = &vprintf;
     close_snarky_print_dest();
+    snarky_print_dest = stdout;
 }
 
 void camlsnark_set_printing_file(char *file) {
-    snarky_printf_deferred = &no_print;
     close_snarky_print_dest();
     snarky_print_dest = fopen(file, "a");
-    if (snarky_print_dest) {
-        snarky_printf_deferred = &file_print;
-    } else {
-        // Fail silently..
-    }
 }
 
-void camlsnark_set_printing_fun(int (*pf)(char*)) {
-    snarky_print_func = pf;
-    snarky_printf_deferred = &snarky_print_to_func;
-    close_snarky_print_dest();
+void camlsnark_set_printing_normal() {
+    snarky_printf_deferred = &normal_printf;
 }
 
-void camlsnark_puts(const char* str) {
-    puts(str);
+void camlsnark_set_printing_json() {
+    snarky_printf_deferred = &json_printf;
 }
 
 void camlsnark_set_profiling(bool b) {
