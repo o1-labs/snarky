908231f151c485fc93c083c30a6f19e6ed26e668
mrmr1993 <mr_1993@hotmail.co.uk>
2019-05-14 17:38:11 +0100

    Use __LOC__ to give meaningful locations for internal errors

--- meja/src/ast_build.ml
+++ meja/src/ast_build.ml
@@ -6,6 +6,19 @@ module Loc = struct
     Location.mkloc x loc
 
   let map x ~f = {Location.loc= x.Location.loc; txt= f x.Location.txt}
+
+  (** Convert the OCaml primitive [__POS__] into a Lexing.position *)
+  let of_prim (file, lnum, cnum, enum) =
+    (* Note: We use a fake value for [pos_bol], since we can't get the true
+             value from [__POS__]. *)
+    { Location.loc_start=
+        {Lexing.pos_fname= file; pos_lnum= lnum; pos_cnum= cnum; pos_bol= 0}
+    ; loc_end=
+        {Lexing.pos_fname= file; pos_lnum= lnum; pos_cnum= enum; pos_bol= 0}
+    ; loc_ghost= false }
+
+  let of_pos (loc_start, loc_end) =
+    { Location.loc_start; loc_end; loc_ghost= false }
 end
 
 module Lid = struct
