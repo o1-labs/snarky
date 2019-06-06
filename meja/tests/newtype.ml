662d529e795d9cc29eb76f8cad25ca55624620f4
mrmr1993 <mr_1993@hotmail.co.uk>
2019-06-06 16:17:16 +0100

    Add some more cases to the newtype test

--- meja/tests/newtype.ml
+++ meja/tests/newtype.ml
@@ -6,3 +6,10 @@ let (f : ('a -> 'a) -> 'a -> 'a) =
 
 let (g : ('a -> 'a) -> 'a -> 'a) =
   fun (type u) (x : u -> u) (y : u) -> (x y : u)
+
+type (_, _) t =
+  | A : (unit, unit) t
+  | B : 'a * (_, 'rest) t -> ('a, 'a -> 'rest) t
+
+let h (type rest) (x : ('a, rest) t) : rest =
+  match x with A -> () | B (x, _) -> x
