dec8a36c951c0f719d3513f9840c07549aa6bad8
Izaak Meckler <ihmeckler@gmail.com>
2019-06-17 05:13:32 -0700

    some updates to install scripts and field tutorial

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 open pin add fold_lib .
 open pin add tuple_lib .
 open pin add bitstring_lib .
@@ -6,16 +7,7 @@ opam pin add ppx_snarky .
 opam pin add meja .
 open pin add snarky .
 
-#!/bin/bash
-mkdir -p ~/bin
-dune build ./snarky.exe
-dune build meja/meja.exe
-
-mv _build/default/snarky.exe ~/bin/snarky
-mv _build/default/meja/meja.exe ~/bin/meja
-
-echo "I've placed the binaries 'snarky' and 'meja' in ~/bin/."
-echo "Please make sure they are on your path by adding the following line to"
+echo "Make sure $OPAM_SWITCH_PREFIX/bin is on your path by adding the following line to"
 echo "one of your ~/.profile, ~/.bash_profile, ~/.bashrc, etc.:"
 echo
-echo 'export PATH="$HOME/bin:$PATH"'
+echo "export PATH=\"$OPAM_SWITCH_PREFIX/bin:$PATH\""
---
+++ all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+which opam
+HAS_OPAM=$?
+
+if [ $HAS_OPAM ] ; then
+  echo "Already had opam installed"
+else
+  sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+fi
+
+git clone https://github.com/o1-labs/snarky.git
+cd snarky
+git checkout zk-workshop
+
+./scripts/depends.sh
+./INSTALL.sh
