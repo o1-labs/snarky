49a66f2a579365e17fb6c10ae830ca3c7363ac65
Izaak Meckler <ihmeckler@gmail.com>
2019-06-17 06:13:01 -0700

    fix all

--- all.sh
+++ all.sh
@@ -6,6 +6,7 @@ if [ $HAS_OPAM ] ; then
   echo "Already had opam installed"
 else
   sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+  opam init
 fi
 
 git clone https://github.com/o1-labs/snarky.git
