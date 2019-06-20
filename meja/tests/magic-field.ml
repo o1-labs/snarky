5d68cd49758b0a9bc1fd446d8a440d0b20ca9576
mrmr1993 <mr_1993@hotmail.co.uk>
2019-06-20 16:46:46 +0100

    Update expected test output

--- meja/tests/magic-field.ml
+++ meja/tests/magic-field.ml
@@ -4,15 +4,31 @@ module Impl =
   Snarky.Snark.Run.Make (Snarky.Backends.Mnt4.Default) (Core_kernel.Unit)
 open Impl
 
-type field_pair = Field.t * Field.t
+include struct
+  type field_pair = Field.Constant.t * Field.Constant.t
 
-type field_pair_alias = field_pair
+  type field_pair_var = Field.t * Field.t
+end
+
+include struct
+  type field_pair_alias = field_pair
+
+  type field_pair_alias_var = field_pair_var
+end
 
 type record_containing_field = {a: int * (int * Field.t)}
 
-type 'a with_param = unit * 'a
+include struct
+  type 'a with_param = unit * 'a
 
-type field_param = Field.t with_param
+  type 'a with_param_var = unit * 'a
+end
+
+include struct
+  type field_param = Field.Constant.t with_param
+
+  type field_param_var = Field.t with_param_var
+end
 
 type variant_containing_field = A of Field.t | B
 
--- meja/tests/prover_test.ml
+++ meja/tests/prover_test.ml
@@ -4,10 +4,10 @@ module Impl =
   Snarky.Snark.Run.Make (Snarky.Backends.Mnt4.Default) (Core_kernel.Unit)
 open Impl
 
-let f x () = ()
+let f x = ()
 
-let ignore (x : Field.t) () = ()
+let ignore (x : Field.t) = ()
 
-let g (x : Field.t) () =
+let g (x : Field.t) =
   let f (x : Field.Constant.t) = () in
   ()
