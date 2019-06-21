f162638355ba4a4ca83b72be51aa1a277615d1e7
mrmr1993 <mr_1993@hotmail.co.uk>
2019-06-21 18:14:49 +0100

    Fix support for GADTs with record arguments

--- meja/src/typechecker.ml
+++ meja/src/typechecker.ml
@@ -367,14 +367,13 @@ let get_ctor (name : lid) env =
               Longident.Lident tdec_ident.txt )
           tdec_ident.loc
       in
-      let typ, params =
+      let typ =
         match ctor.ctor_ret with
-        | Some ({type_desc= Tctor {var_params; _}; _} as typ) ->
-            (typ, var_params)
+        | Some typ ->
+            typ
         | _ ->
-            ( Envi.TypeDecl.mk_typ ~params:tdec_params
-                ~ident:(make_name tdec_ident) decl env
-            , tdec_params )
+            Envi.TypeDecl.mk_typ ~params:tdec_params
+              ~ident:(make_name tdec_ident) decl env
       in
       let args_typ =
         match ctor.ctor_args with
@@ -382,7 +381,7 @@ let get_ctor (name : lid) env =
             Envi.Type.mk
               (Tctor
                  { var_ident= make_name ctor.ctor_ident
-                 ; var_params= params
+                 ; var_params= decl.tdec_params
                  ; var_implicit_params= tdec_implicit_params
                  ; var_decl= decl })
               env
