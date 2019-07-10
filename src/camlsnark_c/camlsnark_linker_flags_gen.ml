acabe4219b226afab10186cf72b161af01644ea7
mrmr1993 <mr_1993@hotmail.co.uk>
2019-07-10 01:31:51 +0100

    Fix flags for OS X

--- src/camlsnark_c/camlsnark_linker_flags_gen.ml
+++ src/camlsnark_c/camlsnark_linker_flags_gen.ml
@@ -2,13 +2,12 @@ open Printf
 module C = Configurator.V1
 
 let () =
-  let cwd = Unix.getcwd () in
   let uname_chan = Unix.open_process_in "uname" in
   let l = input_line uname_chan in
   C.Flags.write_sexp "flags.sexp"
     ( match l with
     | "Darwin" ->
-        [ sprintf "-l%s/libcamlsnark_c_stubs.a" cwd
+        [ sprintf "-lcamlsnark_c_stubs"
         ; "-L/usr/local/opt/openssl/lib"
         ; "-lssl"
         ; "-lcrypto"
