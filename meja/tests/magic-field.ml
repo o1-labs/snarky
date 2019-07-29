d1f92106a3e51a15f3f4ed5569c82a3cdb05741e
mrmr1993 <mr_1993@hotmail.co.uk>
2019-07-29 16:07:13 +0100

    Update magic field test output
    This isn't the desired behaviour, but we don't use the magic field in
    the stdlib anyway.

--- meja/tests/magic-field.ml
+++ meja/tests/magic-field.ml
@@ -17,8 +17,7 @@ type (_, 'field) variant_with_field_param =
   | A : (int, 'field) variant_with_field_param
   | B : ('field, 'field) variant_with_field_param
 
-let add3 (add : 'field -> 'field -> 'field) (x : 'field) (y : 'field)
-    (z : 'field) =
+let add3 (add : field -> field -> field) (x : field) (y : field) (z : field) =
   add (add x y) z
 
 let test_add3 (add : int -> int -> int) = add3 add 1 2 3
