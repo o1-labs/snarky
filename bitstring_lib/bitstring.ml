feebf43151853d31bb0cabdac993ef8b7afdf195
Izaak Meckler <ihmeckler@gmail.com>
2019-08-08 12:11:44 -0700

    add padding functions to bitstring lib

--- bitstring_lib/bitstring.ml
+++ bitstring_lib/bitstring.ml
@@ -10,24 +10,32 @@ module type S = sig
   val init : int -> f:(int -> 'a) -> 'a t
 
   val map : 'a t -> f:('a -> 'b) -> 'b t
+
+  val pad : padding_length:int -> zero:'a -> 'a t -> 'a t
 end
 
 module T = struct
   include List
 
-  let of_list bs = bs
+  let of_list = Fn.id
 end
 
 module Msb_first = struct
   include T
 
   let of_lsb_first = List.rev
+
+  let pad ~padding_length ~zero xs =
+    List.init padding_length ~f:(fun _ -> zero) @ xs
 end
 
 module Lsb_first = struct
   include T
 
   let of_msb_first = List.rev
+
+  let pad ~padding_length ~zero xs =
+    xs @ List.init padding_length ~f:(fun _ -> zero)
 end
 
 let pad_to_triple_list ~default xs =
