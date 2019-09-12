cf517dc47a09f49f4a92346fa3066e685187e99c
mrmr1993 <mr_1993@hotmail.co.uk>
2019-09-12 18:34:14 +0100

    Merkle update infinite stress test

--- examples/merkle_update/merkle_update.ml
+++ examples/merkle_update/merkle_update.ml
@@ -4,7 +4,7 @@
    the Bn128-based snark. *)
 open Core
 open Snarky
-module Snark = Snark.Make (Snark.Backends.Bn128.Default)
+module Snark = Snark.Make (Snarky.Libsnark.Mnt4753.Default)
 module Knapsack = Knapsack.Make (Snark)
 open Snark
 open Let_syntax
@@ -63,103 +63,114 @@ end
 
 module Merkle_tree_checked = Merkle_tree.Checked (Snark) (Hash) (Value)
 
-(* The next thing to do is to specify the input for our snark. *)
-let num_pairs = 5
+module Run (X : sig end) = struct
+  (* The next thing to do is to specify the input for our snark. *)
+  let num_pairs = 5
 
-let depth = 10
+  let depth = 10
 
-let address_typ = Merkle_tree_checked.Address.typ ~depth
+  let address_typ = Merkle_tree_checked.Address.typ ~depth
 
-let input () =
-  let open Data_spec in
-  [ Hash.typ (* [root_start] *)
-  ; Typ.(list ~length:num_pairs (tuple2 Value.typ address_typ))
-    (* [(x_1, addr_1), ..., (x_n, addr_n)]  *)
-  ; Hash.typ
-    (* [root_end] *)
-   ]
+  let input () =
+    let open Data_spec in
+    [ Hash.typ (* [root_start] *)
+    ; Typ.(list ~length:num_pairs (tuple2 Value.typ address_typ))
+      (* [(x_1, addr_1), ..., (x_n, addr_n)]  *)
+    ; Hash.typ
+      (* [root_end] *)
+     ]
 
-(* Now, we write the computation that we want our snark to certify.
+  (* Now, we write the computation that we want our snark to certify.
    A [Checked] computation is paramterized by two types. [Checked.t] is
    a monad, which allows us to use overloaded [let] notation to write verified
    computations in a very natural style.
 *)
-let update_many root_start
-    (updates : (Value.var * Merkle_tree_checked.Address.var) list) root_end :
-    ( unit
-      (* 1. The return type. Here it is [unit], indicating we return
+  let update_many root_start
+      (updates : (Value.var * Merkle_tree_checked.Address.var) list) root_end :
+      ( unit
+        (* 1. The return type. Here it is [unit], indicating we return
         only the [unit] value. *)
-    , (Hash.value, Value.value) Merkle_tree.t
-    (* 2. The prover's state type. Here, we indicate that the prover has
+      , (Hash.value, Value.value) Merkle_tree.t
+      (* 2. The prover's state type. Here, we indicate that the prover has
         access to a Merkle tree using hashes of type [Hash.value] and containing
         values of type [Value.value]. *)
-    )
-    Checked.t =
-  (* We loop over all the updates, updating the prover's Merkle tree as we go. *)
-  let rec go curr_root = function
-    | [] ->
-        return curr_root
-    | (x, addr) :: updates' ->
-        (* We update the Merkle tree, computing the new Merkle root, and repeat. *)
-        let%bind next_root =
-          let%bind prev_value =
-            (* Here we look up the previously stored value in the Merkle tree so that we can use
+      )
+      Checked.t =
+    (* We loop over all the updates, updating the prover's Merkle tree as we go. *)
+    let rec go curr_root = function
+      | [] ->
+          return curr_root
+      | (x, addr) :: updates' ->
+          (* We update the Merkle tree, computing the new Merkle root, and repeat. *)
+          let%bind next_root =
+            let%bind prev_value =
+              (* Here we look up the previously stored value in the Merkle tree so that we can use
             it in authenticating the update. *)
-            exists Value.typ
-              ~compute:
-                As_prover.(
-                  map2 ~f:Merkle_tree.get_exn get_state (read address_typ addr))
+              exists Value.typ
+                ~compute:
+                  As_prover.(
+                    map2 ~f:Merkle_tree.get_exn get_state
+                      (read address_typ addr))
+            in
+            Merkle_tree_checked.update addr ~depth ~root:curr_root
+              ~prev:prev_value ~next:x
           in
-          Merkle_tree_checked.update addr ~depth ~root:curr_root
-            ~prev:prev_value ~next:x
-        in
-        go next_root updates'
-  in
-  let%bind final_root = go root_start updates in
-  (* Finally, after computing the root after all those updates, we assert that it is
+          go next_root updates'
+    in
+    let%bind final_root = go root_start updates in
+    (* Finally, after computing the root after all those updates, we assert that it is
      equal to the specified [root_end]. *)
-  Hash.assert_equal final_root root_end
+    Hash.assert_equal final_root root_end
 
-(* Now that we have specified the computation to produce a snark for, we can actually
+  (* Now that we have specified the computation to produce a snark for, we can actually
    produce the snark. *)
-(* First we generate the keypair. *)
-let keypair = generate_keypair ~exposing:(input ()) update_many
+  (* First we generate the keypair. *)
+  let keypair = generate_keypair ~exposing:(input ()) update_many
 
-(* Next, we can generate a proof on some sample inputs. *)
-(* First the inputs. *)
-let tree_start =
-  let num_entries = 1 lsl depth in
-  let t0 =
-    Merkle_tree.create (Value.random ())
-      ~hash:(fun xo ->
-        Knapsack.hash_to_bits knapsack (Option.value ~default:[] xo) )
-      ~merge:(fun x y -> Knapsack.hash_to_bits knapsack (x @ y))
-  in
-  Merkle_tree.add_many t0
-    (List.init (num_entries - 1) ~f:(fun _ -> Value.random ()))
+  (* Next, we can generate a proof on some sample inputs. *)
+  (* First the inputs. *)
+  let tree_start =
+    let num_entries = 1 lsl depth in
+    let t0 =
+      Merkle_tree.create (Value.random ())
+        ~hash:(fun xo ->
+          Knapsack.hash_to_bits knapsack (Option.value ~default:[] xo) )
+        ~merge:(fun x y -> Knapsack.hash_to_bits knapsack (x @ y))
+    in
+    Merkle_tree.add_many t0
+      (List.init (num_entries - 1) ~f:(fun _ -> Value.random ()))
 
-let updates =
-  let random_addr () = Random.int (Int.pow 2 depth) in
-  List.init num_pairs ~f:(fun _ -> (Value.random (), random_addr ()))
+  let updates =
+    let random_addr () = Random.int (Int.pow 2 depth) in
+    List.init num_pairs ~f:(fun _ -> (Value.random (), random_addr ()))
 
-let tree_end =
-  List.fold_left updates ~init:tree_start ~f:(fun acc (x, addr) ->
-      Merkle_tree.update acc addr x )
+  let tree_end =
+    List.fold_left updates ~init:tree_start ~f:(fun acc (x, addr) ->
+        Merkle_tree.update acc addr x )
 
-let root_start = Merkle_tree.root tree_start
+  let root_start = Merkle_tree.root tree_start
 
-let root_end = Merkle_tree.root tree_end
+  let root_end = Merkle_tree.root tree_end
 
-(* And now the proof. *)
-let proof =
-  prove (Keypair.pk keypair)
-    (input ()) (* The input spec *)
-    tree_start (* The initial state for the prover *) update_many
-    (* The computation to create a snark for *)
-    root_start (* The inputs for the snark *) updates root_end
+  (* And now the proof. *)
+  let proof =
+    prove (Keypair.pk keypair)
+      (input ()) (* The input spec *)
+      tree_start (* The initial state for the prover *) update_many
+      (* The computation to create a snark for *)
+      root_start (* The inputs for the snark *) updates root_end
 
-(* Now we can check that the snark verifies as expected. *)
-let verified =
-  verify proof (Keypair.vk keypair) (input ()) root_start updates root_end
+  (* Now we can check that the snark verifies as expected. *)
+  let verified =
+    verify proof (Keypair.vk keypair) (input ()) root_start updates root_end
+end
 
-let () = printf "Verified: %b\n" verified
+module type Res = sig
+  val verified : bool
+end
+
+let () =
+  while true do
+    let (module X : Res) = (module Run ()) in
+    assert X.verified
+  done
