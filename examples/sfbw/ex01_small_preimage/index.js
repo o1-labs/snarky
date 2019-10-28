48a084e149df966a538a6e6e5e4aaf195068e1ca
Izaak Meckler <ihmeckler@gmail.com>
2019-10-28 15:08:14 -0700

    ex01: update to use snarkyjs-crypto

--- examples/sfbw/ex01_small_preimage/index.js
+++ examples/sfbw/ex01_small_preimage/index.js
@@ -1,17 +1,23 @@
-const Snarky = require("js_snarky");
+const { bn128 } = require('snarkyjs-crypto');
+const Snarky = require('snarkyjs');
 const snarky = new Snarky("./ex01_small_preimage.exe");
 
+const preImage = bn128.Field.ofInt(0x12345678);
+
+const statement = bn128.Hash.hash([preImage]);
+
+const witnessBits = [];
+for (let i = 0; i < 32; ++i) {
+  witnessBits.push(bn128.Field.testBit(preImage, i));
+}
+
 snarky.prove({
-  "statement": ["2550535537361194515906102134927683340979585801486533570983056998398130111650"],
-  "witness": /* 32 bits encoding 0x12345678 */
-    [false, false, false, true, false, false, true,  false,
-     false, false, true,  true, false, true,  false, false,
-     false, true,  false, true, false, true,  true,  false,
-     false, true,  true,  true, true,  false, false, false]
+  "statement": [ bn128.Field.toString(statement) ],
+  "witness": witnessBits
 }).then(function(proof) {
   console.log("Created proof:\n" + proof + "\n");
   return snarky.verify({
-    "statement": ["2550535537361194515906102134927683340979585801486533570983056998398130111650"],
+    "statement": [ bn128.Field.toString(statement) ],
     "proof": proof
   });
 }, console.log).then(function(verified) {
