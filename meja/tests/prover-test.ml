e1493f4cdc5b857f5240f0db1b13a07c86cf88fd
mrmr1993 <mr_1993@hotmail.co.uk>
2019-11-06 16:49:03 +0000

    Update expected test output

--- meja/tests/prover-test.ml
+++ meja/tests/prover-test.ml
@@ -18,15 +18,82 @@ module P = struct
   end
 end
 
-let f x = x
+let f __implicit1__ =
+  let typ x___1015 = Typ.fn x___1015 x___1015 in
+  Snarky.exists (typ __implicit1__)
+    ~compute:
+      (let open As_prover in
+      fun () x -> x)
 
 include struct
-  let g x = f x
+  let g __implicit2__ x =
+    (let typ x___1020 = Typ.fn x___1020 x___1020 in
+     As_prover.read (typ __implicit2__) (f __implicit2__))
+      x
 end
 
-let h x = g x
+let h __implicit6__ __implicit5__ __implicit4__ x =
+  let typ x___1027 = x___1027 in
+  Snarky.exists (typ __implicit6__)
+    ~compute:
+      (let open As_prover in
+      fun () ->
+        g __implicit4__
+          (let typ x___1025 = x___1025 in
+           As_prover.read (typ __implicit5__) x))
 
 let i () =
-  let f x = x in
-  let i = f 15 in
+  let f __implicit7__ =
+    let typ x___1032 = Typ.fn x___1032 x___1032 in
+    Snarky.exists (typ __implicit7__)
+      ~compute:
+        (let open As_prover in
+        fun () x -> x)
+  in
+  let i =
+    let typ = Snarky.Typ.Internal.ref () in
+    Snarky.exists typ
+      ~compute:
+        (let open As_prover in
+        fun () ->
+          (let typ x___1036 = Typ.fn x___1036 x___1036 in
+           As_prover.read
+             (typ (Snarky.Typ.Internal.ref ()))
+             (f (Snarky.Typ.Internal.ref ())))
+            15)
+  in
   i
+
+type nonrec ('a, 'b) either = Fst of 'a | Snd of 'b
+
+let j __implicit12__ __implicit13__ __implicit14__ __implicit10__ b x =
+  let f __implicit11__ =
+    let typ x___1057 = Typ.fn x___1057 (Snarky.Typ.Internal.ref ()) in
+    Snarky.exists (typ __implicit11__)
+      ~compute:
+        (let open As_prover in
+        fun () x ->
+          if
+            let typ x___1055 = x___1055 in
+            As_prover.read (typ __implicit10__) b
+          then Fst x
+          else Snd 15)
+  in
+  let i =
+    let typ = Snarky.Typ.Internal.ref () in
+    Snarky.exists typ
+      ~compute:
+        (let open As_prover in
+        fun () ->
+          (let typ x___1061 = Typ.fn x___1061 (Snarky.Typ.Internal.ref ()) in
+           As_prover.read (typ __implicit13__) (f __implicit12__))
+            (let typ x___1063 = x___1063 in
+             As_prover.read (typ __implicit14__) x))
+  in
+  i
+
+let k __implicit15__ __implicit18__ __implicit16__ (f : field) =
+  j __implicit15__ __implicit16__ Typ.field __implicit18__ true f
+
+let l __implicit19__ __implicit20__ (b : boolean) (b' : boolean) =
+  j __implicit19__ __implicit20__ Typ.boolean Typ.boolean b b'
