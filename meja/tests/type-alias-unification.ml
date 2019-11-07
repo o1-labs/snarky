b8d6394b12941404d96953f3360de048e330a9c8
mrmr1993 <mr_1993@hotmail.co.uk>
2019-11-07 01:24:00 +0000

    Update expected test output, add a TODO

--- meja/tests/type-alias-unification.ml
+++ meja/tests/type-alias-unification.ml
@@ -34,45 +34,9 @@ module Alias_opaque = struct
 end
 
 module Alias_record = struct
-  include struct
-    type nonrec ('a, 'b) u = {a: 'a; b: 'b}
+  type nonrec ('a, 'b) u = {a: 'a; b: 'b}
 
-    and ('a, 'b) u = {a: 'a; b: 'b}
-
-    let u_typ x___1054 x___1053 =
-      { Snarky.Types.Typ.store=
-          (fun {a; b} ->
-            Snarky.Typ_monads.Store.bind (x___1054.Snarky.Types.Typ.store a)
-              ~f:(fun a ->
-                Snarky.Typ_monads.Store.bind
-                  (x___1053.Snarky.Types.Typ.store b) ~f:(fun b ->
-                    Snarky.Typ_monads.Store.return {a; b} ) ) )
-      ; Snarky.Types.Typ.read=
-          (fun {a; b} ->
-            Snarky.Typ_monads.Read.bind (x___1054.Snarky.Types.Typ.read a)
-              ~f:(fun a ->
-                Snarky.Typ_monads.Read.bind (x___1053.Snarky.Types.Typ.read b)
-                  ~f:(fun b -> Snarky.Typ_monads.Read.return {a; b}) ) )
-      ; Snarky.Types.Typ.alloc=
-          Snarky.Typ_monads.Alloc.bind x___1054.Snarky.Types.Typ.alloc
-            ~f:(fun a ->
-              Snarky.Typ_monads.Alloc.bind x___1053.Snarky.Types.Typ.alloc
-                ~f:(fun b -> Snarky.Typ_monads.Alloc.return {a; b}) )
-      ; Snarky.Types.Typ.check=
-          (fun {a; b} ->
-            Snarky.Checked.bind (x___1054.Snarky.Types.Typ.check a)
-              ~f:(fun () ->
-                Snarky.Checked.bind (x___1053.Snarky.Types.Typ.check b)
-                  ~f:(fun () -> Snarky.Checked.return ()) ) ) }
-  end
-
-  include struct
-    type nonrec ('a, 'b) v = ('a, 'a) u
-
-    and ('a, 'b) v = ('a, 'a) u
-
-    let v_typ x___1059 x___1058 = u_typ x___1059 x___1059
-  end
+  type nonrec ('a, 'b) v = ('a, 'a) u
 
   let f (x : (int, int) v) : (int, bool) v = x
 
