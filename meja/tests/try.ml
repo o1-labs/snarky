35d1cfb14dc949333d7958623ec2b05168ddb9b2
mrmr1993 <mr_1993@hotmail.co.uk>
2020-01-15 17:36:03 +0000

    Add tests for try

---
+++ meja/tests/try.ml
@@ -0,0 +1,12 @@
+module Impl = Snarky.Snark.Make (Snarky.Backends.Mnt4.Default)
+open Impl
+
+let (x : unit -> int) = fun () -> try failwith "oh dear" with Failure _ -> 15
+
+let (y : bool -> string) =
+ fun b -> try if b then "" else failwith "" with Failure _ -> ""
+
+exception Other of int * bool
+
+let (z : unit -> int) =
+ fun () -> try 15 with Failure _ -> 12 | Other (i, _) -> i
