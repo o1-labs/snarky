acfa3749082ee298e52e5a3eae246601bc84e338
mrmr1993 <mr_1993@hotmail.co.uk>
2020-06-15 22:21:25 +0100

    Add build-opam script, shortcut if the switch is up-to-date

---
+++ scripts/build-opam.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Check if the switch is up-to-date
+opam switch export opam.test
+if diff opam.test opam.export; then
+    exit 0
+fi
+
+opam init
+opam update
+opam switch create 4.07.1 || true
+opam switch switch
+opam switch import opam.export
