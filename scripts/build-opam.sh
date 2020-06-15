310ef5d1d28afd13c090c1b40b5d14c42ad74e10
mrmr1993 <mr_1993@hotmail.co.uk>
2020-06-15 22:36:20 +0100

    Grumble

--- scripts/build-opam.sh
+++ scripts/build-opam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eo
 
 # Check if the switch is up-to-date
 opam switch export opam.test
@@ -11,5 +11,5 @@ fi
 opam init
 opam update
 opam switch create 4.07.1 || true
-opam switch switch
+opam switch 4.07.1
 opam switch import opam.export
