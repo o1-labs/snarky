ea9af8d09e4462d7d94bd709df0812d0e153ac42
Izaak Meckler <ihmeckler@gmail.com>
2020-06-30 06:53:13 -0700

    add iter and map2 for tuple types (#497)
    * change the sponge operations interface, add a proper sponge, add a sponge that outputs bits
    
    * fix compilation
    
    * add bn382 sponge constants
    
    * add copy and state to interface
    
    * bn382 params in sage file
    
    * various sponge tweaks
    
    * fix bad merge
    
    * new field intf
    
    * expose vector as pointer
    
    * add two optimizations to cvar, move to_constant_and_terms function there
    
    * add constraint system interfaces
    
    * functor for preparing the libsnark constraint systems from the bindings
    
    * existential constraint system
    
    * remove some stringification functions, add function for generating public input to snark_intf
    
    * remove the libsnark constraint system specific code from backend extended
    
    * update checked runner to work with existential constraint sysetm
    
    * update run_state to have existential constraint system
    
    * reorganize the code in libsnark.ml
    
    * make snark0 compile, make && friendlier to marlin, add a unit test for all, add generate_public_input function
    
    * make tuple typ behavior go in left to right order
    
    * remove zero constant term
    
    * add if_
    
    * update group map
    
    * tweaks to group map
    
    * rename to scale_fast
    
    * squeeze_field function on sponge
    
    * opaqify vector
    
    * test_eq in typ
    
    * _
    
    * constants for tweedle sponges
    
    * no ppx coda in groupmap
    
    * reconfigure interface and add new sponge constants
    
    * add iter and map2 for everything
    
    Co-authored-by: Matthew Ryan <MR_1993@hotmail.co.uk>

--- tuple_lib/triple.ml
+++ tuple_lib/triple.ml
@@ -3,3 +3,7 @@ open Core_kernel
 type 'a t = 'a * 'a * 'a [@@deriving bin_io, sexp, eq, compare]
 
 let map (x1, x2, x3) ~f = (f x1, f x2, f x3)
+
+let map2 (x1, x2, x3) (y1, y2, y3) ~f = (f x1 y1, f x2 y2, f x3 y3)
+
+let iter (x1, x2, x3) ~f = f x1 ; f x2 ; f x3
