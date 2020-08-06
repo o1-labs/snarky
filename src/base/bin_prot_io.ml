2062d610c61b7b1d0b715832d9b1e844eb4baf0f
mrmr1993 <mr_1993@hotmail.co.uk>
2020-08-06 19:56:24 +0100

    Reformat

--- src/base/bin_prot_io.ml
+++ src/base/bin_prot_io.ml
@@ -8,6 +8,8 @@ let read m filename =
 
 let write m filename ~data =
   Out_channel.with_file filename ~binary:true ~f:(fun channel ->
-      let buffer = Binable.to_string m data |> Bytes.unsafe_of_string_promise_no_mutation in
-      Out_channel.output channel ~buf:buffer ~pos:0
-        ~len:(Bytes.length buffer) )
+      let buffer =
+        Binable.to_string m data |> Bytes.unsafe_of_string_promise_no_mutation
+      in
+      Out_channel.output channel ~buf:buffer ~pos:0 ~len:(Bytes.length buffer)
+  )
