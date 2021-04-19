762e5ed4d2b1f80fa8020f1bed8a3be69021d8b1
mrmr1993 <mr_1993@hotmail.co.uk>
2021-04-19 19:51:33 +0100

    Add test for typ_ppx

---
+++ ppx/test/testcases/typ_ppx.ml
@@ -0,0 +1,56 @@
+module Field = struct
+  type t = int
+
+  let to_field_elements i = [|i|]
+
+  let size_in_field_elements = 1
+
+  let of_field_elements_indexed ~offset x =
+    let x = x.(!offset) in
+    incr offset ; x
+
+  let of_field_elements = function
+    | [|x|] ->
+        x
+    | _ ->
+        raise (Invalid_argument "Field.of_field_elements")
+end
+
+module Boolean = struct
+  type t = bool
+
+  let to_field_elements b = [|(if b then 1 else 0)|]
+
+  let size_in_field_elements = 1
+
+  let of_field_elements_indexed ~offset x =
+    let x = x.(!offset) in
+    incr offset ;
+    match x with 0 -> false | 1 -> true | _ -> assert false
+
+  let of_field_elements = function
+    | [|x|] ->
+        x
+    | _ ->
+        raise (Invalid_argument "Boolean.of_field_elements")
+end
+
+type ('a, 'b) t = {a: 'a * Field.t; b: 'b * Boolean.t * Field.t}
+[@@deriving snarky_typ]
+
+module type S = sig
+  module M : sig
+    type ('a, 'b) t [@@deriving snarky_typ]
+  end
+
+  type t [@@deriving snarky_typ]
+end
+
+module Test : S = struct
+  module M = struct
+    type ('a, 'b) t = {a: 'a * Field.t; b: 'b * Boolean.t * Field.t}
+    [@@deriving snarky_typ]
+  end
+
+  include Field
+end
