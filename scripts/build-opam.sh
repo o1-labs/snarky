428ae90717ee6b754d37941e7212b7ea46e74880
mrmr1993 <mr_1993@hotmail.co.uk>
2021-05-28 19:49:07 +0100

    Re-order opam setup script

--- scripts/build-opam.sh
+++ scripts/build-opam.sh
@@ -2,6 +2,12 @@
 
 set -xeo
 
+opam init
+eval $(opam env)
+opam switch create 4.07.1 || true
+opam switch 4.07.1
+opam update
+
 # Generate opam.test from the current opam switch
 opam switch export opam.test
 # Check if the switch is up-to-date
@@ -9,8 +15,4 @@ if diff opam.test opam.export; then
     exit 0
 fi
 
-opam init
-opam update
-opam switch create 4.07.1 || true
-opam switch 4.07.1
 opam switch import opam.export
