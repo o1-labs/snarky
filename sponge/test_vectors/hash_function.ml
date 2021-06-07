000389f253f24a33110f401839125da330cadbda
David Wong <davidwong.crypto@gmail.com>
2021-06-07 13:04:56 -0700

    [sponge] added test vectors for three_wire

---
+++ sponge/test_vectors/hash_function.ml
@@ -0,0 +1,106 @@
+open Core_kernel
+
+(* *************** *
+ *    our field    *
+ * *************** *)
+
+module Field = struct
+  include Snarkette.Pasta.Fp
+
+  (* Converts a byterray into a [Field.t], raises an exception if the number obtained is larger than the order *)
+  let of_bytes (bytearray: string) : t =
+    let aux i acc c =
+      let big = Nat.of_int @@ int_of_char c in
+      let offset = Nat.shift_left big (Int.( * ) i 8) in
+      Nat.(acc + offset)
+    in
+    let zero = Nat.of_int 0 in
+    let big = String.foldi bytearray ~init:zero ~f:aux in
+    let one = Nat.of_int 1 in
+    if Nat.(order - one < big) then
+      failwith "the given field is larger than the order" ;
+    of_bigint big
+
+  (* Converts an hexadecimal string into a [Field.t], raises an exception if the number obtained is larger than the order *)
+  let of_hex (hexstring: string) : t =
+    let bytearray : string = Hex.decode hexstring |> Bytes.to_string in
+    of_bytes bytearray
+
+  (* Converts a field element into a bytearray (encoding the field element in little-endian) *)
+  let to_bytes (field: t) : bytes =
+    (* taken from src/lib/pickles *)
+    let bits_to_bytes bits =
+      let byte_of_bits bs =
+        List.foldi bs ~init:0 ~f:(fun i acc b ->
+            if b then acc lor (1 lsl i) else acc )
+        |> Char.of_int_exn
+      in
+      List.map
+        (List.groupi bits ~break:(fun i _ _ -> i mod 8 = 0))
+        ~f:byte_of_bits
+      |> Bytes.of_char_list
+    in
+    let bytearray = to_bits field |> bits_to_bytes in
+    bytearray
+
+  (* Converts a field element into an hexadecimal string (encoding the field element in little-endian) *)
+  let to_hex (field: t) : string =
+    let bytearray : bytes = to_bytes field in
+    Hex.encode bytearray
+end
+
+(* ********************** *
+ * our permutation Config *
+ * ********************** *)
+
+module Config = struct
+  module Field = Field
+
+  let rounds_full = 63
+
+  let initial_ark = true
+
+  let rounds_partial = 0
+
+  let to_the_alpha x =
+    let open Field in
+    let x_2 = x * x in
+    let x_4 = x_2 * x_2 in
+    let x_5 = x_4 * x in
+    x_5
+
+  module Operations = struct
+    let add_assign ~state i x = Field.(state.(i) <- state.(i) + x)
+
+    let apply_affine_map (matrix, constants) v =
+      let dotv row =
+        Array.reduce_exn (Array.map2_exn row v ~f:Field.( * )) ~f:Field.( + )
+      in
+      let res = Array.map matrix ~f:dotv in
+      Array.map2_exn res constants ~f:Field.( + )
+
+    let copy a = Array.map a ~f:Fn.id
+  end
+end
+
+(* ***************** *
+ *   hash function   *
+ * ***************** *)
+
+module Hash = struct
+  include Sponge.Make_hash (Sponge.Poseidon (Config))
+
+  let params : Field.t Sponge.Params.t =
+    Sponge.Params.(map pasta_p ~f:Field.of_string)
+
+  let hash ?init = hash ?init params
+  
+  (* input is an array of field elements encoded as hexstrings *)
+  let hash_field_elems (field_elems : string list) : string =
+    let input : Field.t array =
+      if List.length field_elems = 0 then [||] else
+      Array.of_list @@ List.map field_elems ~f:Field.of_hex
+    in
+    let digest = hash ~init:initial_state input in
+    Field.to_hex digest
+end
---
+++ sponge/test_vectors/main.ml
@@ -0,0 +1,35 @@
+open Core_kernel
+
+type test_vector = {
+  input: string list;
+  output: string;
+}
+
+type test_vectors = {
+  name: string;
+  test_vectors: test_vector list;
+}
+
+let parse_test_vectors filepath =
+  let json = Yojson.Basic.from_file filepath in
+  let open Yojson.Basic.Util in
+  let name = json |> member "name" |> to_string in 
+  let test_vectors = json |> member "test_vectors" |> to_list in
+  let json_to_test_vector test_vector = 
+    let input = test_vector |> member "input" |> to_list |> filter_string in
+    let output = test_vector |> member "output" |> to_string in
+    {input; output}
+  in
+  let test_vectors = List.map test_vectors ~f:json_to_test_vector in
+  {name; test_vectors}
+
+let () = 
+  let cur_dir = Sys.argv.(0) |> Filename.dirname in 
+  let test_vector_file = Filename.concat cur_dir "three_wire.json" in
+  let test_vectors = parse_test_vectors test_vector_file in 
+  assert (test_vectors.name = "three_wire");
+  let check_test_vector test_vector = 
+    let digest = Hash_function.Hash.hash_field_elems test_vector.input in 
+    assert (digest = test_vector.output)
+  in
+  List.iter test_vectors.test_vectors ~f:check_test_vector
