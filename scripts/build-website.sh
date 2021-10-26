69cb8592bc6d7bc9d4891980fd02231e78cafa62
David Wong <davidwong.crypto@gmail.com>
2021-10-26 14:10:55 -0700

    [CI] use the website script instead

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-rm -r website/website/pages/api || true
-cp -r -T _build/default/_doc/_html/ website/website/pages/api
+rm -rf website/website/pages/api || true
+cp -r _build/default/_doc/_html/ website/website/pages/api
 echo "* binary" > website/website/pages/api/.gitattributes
 rm website/website/pages/api/highlight.pack.js
 
 # Fix css and js URLs
 pushd website/website/pages/api;
-for filename in $(find -name '*.html'); do
-  sed -e 's/\.\.\/odoc.css/..\/..\/..\/snarky\/css\/odoc.css/' -e 's/\.\.\/highlight.pack.js/..\/..\/..\/snarky\/js\/highlight.pack.js/' -i "$filename"
+for filename in $(find . -name '*.html'); do
+  sed -e 's/\.\.\/odoc.css/..\/..\/..\/snarky\/css\/odoc.css/' -e 's/\.\.\/highlight.pack.js/..\/..\/..\/snarky\/js\/highlight.pack.js/' -i'' "$filename"
 done
