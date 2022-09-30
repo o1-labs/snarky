3b7faf37e9170773fc4c103867045c26eceb8c94
mrmr1993 <mr_1993@hotmail.co.uk>
2022-09-30 14:09:09 +0100

    Move checked Make back to checked module

---
+++ src/base/checked.ml
@@ -0,0 +1,90 @@
+open Core_kernel
+module Make
+    (Basic : Checked_intf.Basic)
+    (As_prover : As_prover_intf.Basic
+                   with type ('a, 'f) t = ('a, 'f) Basic.Types.As_prover.t
+                    and type 'f field := 'f Basic.field) :
+  Checked_intf.S
+    with module Types = Basic.Types
+    with type 'f field = 'f Basic.field = struct
+  include Basic
+
+  let request_witness (typ : ('var, 'value, 'f field) Types.Typ.t)
+      (r : ('value Request.t, 'f field) As_prover.t) =
+    let%map h = exists typ (Request r) in
+    Handle.var h
+
+  let request ?such_that typ r =
+    match such_that with
+    | None ->
+        request_witness typ (As_prover.return r)
+    | Some such_that ->
+        let open Let_syntax in
+        let%bind x = request_witness typ (As_prover.return r) in
+        let%map () = such_that x in
+        x
+
+  let exists_handle ?request ?compute typ =
+    let provider =
+      let request =
+        Option.value request ~default:(As_prover.return Request.Fail)
+      in
+      match compute with
+      | None ->
+          Types.Provider.Request request
+      | Some c ->
+          Types.Provider.Both (request, c)
+    in
+    exists typ provider
+
+  let exists ?request ?compute typ =
+    let%map h = exists_handle ?request ?compute typ in
+    Handle.var h
+
+  type response = Request.response
+
+  let unhandled = Request.unhandled
+
+  type request = Request.request =
+    | With :
+        { request : 'a Request.t; respond : 'a Request.Response.t -> response }
+        -> request
+
+  let handle t k = with_handler (Request.Handler.create_single k) t
+
+  let handle_as_prover t k =
+    let handler = ref None in
+    let%bind () =
+      as_prover
+        As_prover.(
+          let%map h = k in
+          handler := Some h)
+    in
+    handle t (fun request -> (Option.value_exn !handler) request)
+
+  let assert_ ?label c =
+    add_constraint (List.map c ~f:(fun c -> Constraint.override_label c label))
+
+  let assert_r1cs ?label a b c = assert_ (Constraint.r1cs ?label a b c)
+
+  let assert_square ?label a c = assert_ (Constraint.square ?label a c)
+
+  let assert_all =
+    let map_concat_rev xss ~f =
+      let rec go acc xs xss =
+        match (xs, xss) with
+        | [], [] ->
+            acc
+        | [], xs :: xss ->
+            go acc xs xss
+        | x :: xs, _ ->
+            go (f x :: acc) xs xss
+      in
+      go [] [] xss
+    in
+    fun ?label cs ->
+      add_constraint
+        (map_concat_rev ~f:(fun c -> Constraint.override_label c label) cs)
+
+  let assert_equal ?label x y = assert_ (Constraint.equal ?label x y)
+end
