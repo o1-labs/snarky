7635135515cd6293ca0cc2be2e3f2eb7435de2c6
David Wong <davidwong.crypto@gmail.com>
2022-12-09 00:54:19 -0800

    merge as_prover and as_prover0

--- src/base/as_prover0.ml
+++ src/base/as_prover0.ml
@@ -80,3 +80,22 @@ module Handle = struct
   let value (t : ('var, 'value) Handle.t) : ('value, 'field) t =
    fun _ -> Option.value_exn t.value
 end
+
+module type Extended = sig
+  type field
+
+  include As_prover_intf.Basic with type 'f field := field
+
+  type 'a t = ('a, field) As_prover0.t
+end
+
+module Make_extended (Env : sig
+  type field
+end)
+(As_prover : As_prover_intf.Basic with type 'f field := Env.field) =
+struct
+  include Env
+  include As_prover
+
+  type 'a t = ('a, field) As_prover0.t
+end
