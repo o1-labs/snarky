f92e6c9a01df0daf55c41c29b95747c9eb4beee2
mrmr1993 <mr_1993@hotmail.co.uk>
2024-02-11 04:19:41 +0000

    Adopt helper function into module

--- src/base/runners.ml
+++ src/base/runners.ml
@@ -164,40 +164,40 @@ struct
       Field.Vector.emplace_back primary_input x ;
       v
 
-    let allocate_public_inputs :
-        type input_var input_value output_var output_value.
-           int ref
-        -> input_typ:
-             ( input_var
-             , input_value
-             , field
-             , (unit, field) Checked.Types.Checked.t )
-             Types.Typ.typ
-        -> return_typ:
-             ( output_var
-             , output_value
-             , field
-             , (unit, field) Checked.Types.Checked.t )
-             Types.Typ.t
-        -> input_var * output_var =
-     fun next_input ~input_typ:(Typ input_typ) ~return_typ:(Typ return_typ) ->
-      (* allocate variables for the public input and the public output *)
-      let alloc_input
-          { Types0.Typ.var_of_fields
-          ; size_in_field_elements
-          ; constraint_system_auxiliary
-          ; _
-          } =
-        var_of_fields
-          ( Core_kernel.Array.init size_in_field_elements ~f:(fun _ ->
-                alloc_var next_input () )
-          , constraint_system_auxiliary () )
-      in
-      let var = alloc_input input_typ in
-      let retval = alloc_input return_typ in
-      (var, retval)
-
     module Constraint_system_builder = struct
+      let allocate_public_inputs :
+          type input_var input_value output_var output_value.
+             int ref
+          -> input_typ:
+               ( input_var
+               , input_value
+               , field
+               , (unit, field) Checked.Types.Checked.t )
+               Types.Typ.typ
+          -> return_typ:
+               ( output_var
+               , output_value
+               , field
+               , (unit, field) Checked.Types.Checked.t )
+               Types.Typ.t
+          -> input_var * output_var =
+       fun next_input ~input_typ:(Typ input_typ) ~return_typ:(Typ return_typ) ->
+        (* allocate variables for the public input and the public output *)
+        let alloc_input
+            { Types0.Typ.var_of_fields
+            ; size_in_field_elements
+            ; constraint_system_auxiliary
+            ; _
+            } =
+          var_of_fields
+            ( Core_kernel.Array.init size_in_field_elements ~f:(fun _ ->
+                  alloc_var next_input () )
+            , constraint_system_auxiliary () )
+        in
+        let var = alloc_input input_typ in
+        let retval = alloc_input return_typ in
+        (var, retval)
+
       let r1cs_h :
           type a checked input_var input_value retval.
              run:(a, checked) Runner.run
