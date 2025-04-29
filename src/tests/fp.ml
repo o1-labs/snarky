3a84d37fc3ba0e8233ad0fbc83171b75af5fe0fb
martyall <martyall@protonmail.com>
2025-04-29 14:42:28 -0700

    backend module defined in snarky_tests compiles

--- src/tests/fp.ml
+++ src/tests/fp.ml
@@ -1,3 +1,4 @@
+open Core
 module Bignum_bigint = Bigint
 
 let rec extended_euclidean a b =
@@ -40,7 +41,7 @@ end
 
 module Fp (P : sig
   val order : Bignum_bigint.t
-end) : Snarky_intf.Field.S = struct
+end) : Snarky_intf.Field.S with type t = Bignum_bigint.t = struct
   type t = Bignum_bigint.t
 
   include Serialize
@@ -67,7 +68,7 @@ end) : Snarky_intf.Field.S = struct
     let euler = Bignum_bigint.((P.order - of_int 1) / of_int 2) in
     fun x -> Bignum_bigint.(equal (x ** euler) one)
 
-  let sqrt =
+  let sqrt _ =
     failwith "sqrt not implemented, not possible for arbitrary finite fields"
 
   let size_in_bits = Bignum_bigint.to_zarith_bigint P.order |> Z.log2
@@ -77,8 +78,6 @@ end) : Snarky_intf.Field.S = struct
   let random _ = Bignum_bigint.(random P.order)
 
   module Vector = struct
-    open Core
-
     type elt = t
 
     type t = elt Deque.t
@@ -93,14 +92,142 @@ end) : Snarky_intf.Field.S = struct
   end
 end
 
-module Bigint : Snarky_intf.Bigint_intf.S = struct
-  include Bignum_bigint
+module Biginteger (P : sig
+  val order : Bignum_bigint.t
+end) :
+  Snarky_intf.Bigint_intf.Extended
+    with type t = Bignum_bigint.t
+     and type field := Bignum_bigint.t = struct
+  type t = Bignum_bigint.t
 
-  let of_int = Bignum_bigint.of_int
+  include Serialize
 
-  let to_int = Bignum_bigint.to_int
+  let of_field x = x
 
-  let of_string = Bignum_bigint.of_string
+  let test_bit x = Z.testbit (Bignum_bigint.to_zarith_bigint x)
 
-  let to_string = Bignum_bigint.to_string
+  let to_field x = x
+
+  let compare = Bignum_bigint.compare
+
+  let of_numeral x ~base =
+    Z.of_string_base base x |> Bignum_bigint.of_zarith_bigint
+
+  let of_decimal_string = of_numeral ~base:10
+
+  let length_in_bytes =
+    let max = Bignum_bigint.(P.order - of_int 1) in
+    Bignum_bigint.to_zarith_bigint max |> Z.numbits |> fun x -> x / 8
+
+  let of_data _ = failwith "Biginteger.of_data not implemented"
+end
+
+module Backend : Snarky.Backend_intf.S = struct
+  module P = struct
+    let order = Bignum_bigint.of_int 5
+  end
+
+  module Field = Fp (P)
+  module Bigint = Biginteger (P)
+
+  let field_size = P.order
+
+  module Cvar = Snarky.Cvar.Make (Field)
+
+  module Constraint = struct
+    type t =
+      | Boolean of Field.t Snarky.Cvar.t
+      | Equal of Field.t Snarky.Cvar.t * Field.t Snarky.Cvar.t
+      | Square of Field.t Snarky.Cvar.t * Field.t Snarky.Cvar.t
+      | R1CS of
+          Field.t Snarky.Cvar.t * Field.t Snarky.Cvar.t * Field.t Snarky.Cvar.t
+    [@@deriving sexp]
+
+    let boolean x = Boolean x
+
+    let equal x y = Equal (x, y)
+
+    let r1cs x y z = R1CS (x, y, z)
+
+    let square x y = Square (x, y)
+
+    let eval (t : t) (f : Field.t Snarky.Cvar.t -> Field.t) =
+      match t with
+      | Boolean x ->
+          f x == Field.one
+      | Equal (x, y) ->
+          f x == f y
+      | Square (x, y) ->
+          Field.mul (f x) (f x) == f y
+      | R1CS (x, y, z) ->
+          Field.mul (f x) (f y) == f z
+
+    (* This is a placeholder for the actual logging function *)
+
+    let log_constraint (t : t) (f : Field.t Snarky.Cvar.t -> Field.t) : string =
+      match t with
+      | Boolean x ->
+          Printf.sprintf "Boolean(%s)" (Field.to_string (f x))
+      | Equal (x, y) ->
+          Printf.sprintf "Equal(%s, %s)"
+            (f x |> Field.to_string)
+            (f y |> Field.to_string)
+      | Square (x, y) ->
+          Printf.sprintf "Square(%s, %s)"
+            (f x |> Field.to_string)
+            (f y |> Field.to_string)
+      | R1CS (x, y, z) ->
+          Printf.sprintf "R1CS(%s, %s, %s)"
+            (f x |> Field.to_string)
+            (f y |> Field.to_string)
+            (f z |> Field.to_string)
+
+    let t_of_sexp _ = failwith "t_of_sexp not implemented"
+
+    let sexp_of_t _ = failwith "sexp_of_t not implemented"
+  end
+
+  module R1CS_constraint_system = struct
+    module Field = Field
+
+    type constraint_ = Constraint.t
+
+    type t =
+      { public_input_size : int Core_kernel.Set_once.t
+      ; auxiliary_input_size : int Core_kernel.Set_once.t
+      ; constraints : constraint_ Deque.t
+      }
+
+    let create () =
+      { public_input_size = Core_kernel.Set_once.create ()
+      ; auxiliary_input_size = Core_kernel.Set_once.create ()
+      ; constraints = Deque.create ()
+      }
+
+    let finalize _ = ()
+
+    let add_constraint t c = Deque.enqueue_back t.constraints c
+
+    let digest _ = failwith "R1CSSystem.digest not implemented"
+
+    let set_primary_input_size t size =
+      Core_kernel.Set_once.set_exn t.public_input_size [%here] size
+
+    let set_auxiliary_input_size t size =
+      Core_kernel.Set_once.set_exn t.auxiliary_input_size [%here] size
+
+    let get_public_input_size t = t.public_input_size
+
+    let get_rows_len t = Deque.length t.constraints
+  end
+
+  module Run_state = Snarky.Run_state.Make (struct
+    type field = Field.t
+
+    type constraint_ = Constraint.t
+  end)
+end
+
+module Snark = struct
+  include Snarky.Snark.Make (Backend)
 end
